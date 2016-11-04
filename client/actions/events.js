67e409996cdf6bd2366df845393464da6b94a2a1
Mark Lewis <marklewis@marks-mbp.localdomain>
2016-11-04 19:11:51 +0100

    feature(client): add location saving, though not complete

--- client/actions/events.js
+++ client/actions/events.js
@@ -13,6 +13,76 @@ const requestEvents = () => ({
 export const addEvents = (events) => ({
     type: 'ADD_EVENTS', events
 })
+export const saveLocation = (newLocation) => (
+    (dispatch, getState, { api }) => {
+        // check if the newLocation is already saved in internal
+        // locations resources, if so just return the name and
+        // guid (but should be a promise)
+            
+        // Map location.gmaps fields to formattedLocation
+        // TODO: refactor with a loop or map function
+        let addressComponents = newLocation.gmaps.address_components
+        let streetNumber = addressComponents.find((component) => 
+            component.types.indexOf('street_number') > -1
+        )
+        let route = addressComponents.find((component) => 
+            component.types.indexOf('route') > -1
+        )
+        let locality = addressComponents.find((component) => 
+            component.types.indexOf('locality') > -1
+        )
+        let subLocality1 = addressComponents.find((component) => 
+            component.types.indexOf('sublocality_level_1') > -1
+        )
+        let subLocality2 = addressComponents.find((component) => 
+            component.types.indexOf('sublocality_level_2') > -1
+        )
+        let subLocality3 = addressComponents.find((component) => 
+            component.types.indexOf('sublocality_level_3') > -1
+        )
+        let subLocality4 = addressComponents.find((component) => 
+            component.types.indexOf('sublocality_level_4') > -1
+        )
+        let subLocality5 = addressComponents.find((component) => 
+            component.types.indexOf('sublocality_level_5') > -1
+        )
+        let country = addressComponents.find((component) => 
+            component.types.indexOf('country') > -1
+        )
+        let postalCode = addressComponents.find((component) => 
+            component.types.indexOf('postal_code') > -1
+        )
+        let area = subLocality1.short_name
+        area += (subLocality2) ? ', ' + subLocality2.short_name : ''
+        area += (subLocality3) ? ', ' + subLocality3.short_name : ''
+        area += (subLocality4) ? ', ' + subLocality4.short_name : ''
+        area += (subLocality5) ? ', ' + subLocality5.short_name : ''
+        let address = {
+            line: [ streetNumber.short_name + ' ' + route.short_name ],
+            locality: locality.short_name,
+            area: area,
+            country: country.short_name,
+            postal_code: postalCode.short_name,
+            external: { 
+                gmaps: newLocation.gmaps
+            }
+        }
+        let lat = newLocation.gmaps.geometry.location.lat()
+        let lng = newLocation.gmaps.geometry.location.lng()
+        let formattedLocation = {
+            unique_name: newLocation.gmaps.formatted_address,
+            name: newLocation.gmaps.formatted_address,
+            address: address,
+            position: {
+                latitude: lat,
+                longitude: lng
+            }
+        }
+
+        return api('locations').save({}, formattedLocation)
+        
+    }
+)
 export const saveEvent = (newEvent) => (
     (dispatch, getState, { api }) => {
         let events = getState().events
@@ -26,15 +96,27 @@ export const saveEvent = (newEvent) => (
         newEvent = pickBy(newEvent, (v, k) => (!k.startsWith('_')))
         // save the timezone. This is useful for recurring events
         newEvent.dates.tz = moment.tz.guess()
-        // send the event on the backend
-        return api('events').save(original, newEvent)
-        // add the event to the store
-        .then(data => {
-            dispatch(addEvents(data._items || [data]))
-            // notify the end of the action and reset the form
-            dispatch({ type: 'EVENT_SAVE_SUCCESS' })
-            // hide the modal
-            return dispatch(hideModal())
+        // save location before saving event
+        dispatch(saveLocation(newEvent.location[0].name))
+        .then(newLocation => {
+            // map location fields
+            newEvent.location = [
+                { 
+                    name: newLocation.name,
+                    qcode: newLocation.guid
+                }
+            ]
+
+            // send the event on the backend
+            return api('events').save(original, newEvent)
+            // add the event to the store
+            .then(data => {
+                dispatch(addEvents(data._items || [data]))
+                // notify the end of the action and reset the form
+                dispatch({ type: 'EVENT_SAVE_SUCCESS' })
+                // hide the modal
+                return dispatch(hideModal())
+            })
         })
     }
 )
