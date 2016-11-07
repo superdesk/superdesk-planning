fa5e5c5e43adbf4ebc091beedba184f131e64b1d
Mark Lewis <marklewis@Marks-MacBook-Pro.local>
2016-11-07 11:28:41 +0100

    chore(client): rename Planning controller

--- client/index.js
+++ client/index.js
@@ -6,7 +6,7 @@ export default angular.module('superdesk.planning', [])
             scope: {},
             bindToController: true,
             controllerAs: 'vm',
-            controller: ctrl.PlanningModalController
+            controller: ctrl.PlanningController
         })
     )
     .controller('PlanningSettingsController', ctrl.PlanningSettingsController)
