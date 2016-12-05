4aa967a3c4d887078f76d63b9037648c74c17bfc
Edouard <edou4rd@gmail.com>
2016-12-05 16:59:02 +0100

    Open the planning editor
    It's empty for the moment

---
+++ client/components/EditPlanningPanelContainer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import * as actions from '../actions'
+
+class EditPlanningPanel extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div className="Planning__edit-planning">
+                <header className="subnav">
+                    <h3 className="subnav__page-title">Created 5min ago by Edouard</h3>
+                    <a onClick={this.props.closePlanningEditor} className="close">
+                        <i className="icon-close-small"></i>
+                    </a>
+                </header>
+            </div>
+        )
+    }
+}
+
+EditPlanningPanel.propTypes = {
+    closePlanningEditor: React.PropTypes.func.isRequired
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    closePlanningEditor: () => dispatch(actions.closePlanningEditor())
+})
+
+export const EditPlanningPanelContainer = connect(null, mapDispatchToProps)(EditPlanningPanel)
