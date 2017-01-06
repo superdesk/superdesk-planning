95e38c425711f5e7bbe0c3d89311dc52ce332721
Edouard <edou4rd@gmail.com>
2017-01-06 12:38:25 +0100

    Simplify store creation in tests

--- client/components/tests/AddEventContainer_test.jsx
+++ client/components/tests/AddEventContainer_test.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import { AddEventContainer, AddEvent } from '../AddEventContainer'
-import { createStore } from '../../utils'
+import { createTestStore } from '../../utils'
 import { Provider } from 'react-redux'
 import * as actions from '../../actions'
 
 describe('<AddEventContainer />', () => {
     it('open the modal', () => {
-        let store = createStore({ testMode: true })
+        let store = createTestStore()
         const wrapper = mount(<Provider store={store}><AddEventContainer /></Provider>)
         expect(wrapper.find(AddEvent).props().show).toBe(false)
         store.dispatch(actions.showModal({ modalType: 'EDIT_EVENT', modalProps: { event: {} } }))
