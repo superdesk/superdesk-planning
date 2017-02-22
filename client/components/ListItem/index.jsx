2cd002f7980762ac9dbb4c775b1d35456fc80631
Edouard Richard <edou4rd@gmail.com>
2017-02-22 10:52:58 +0100

    planning context menu removed

--- client/components/ListItem/index.jsx
+++ client/components/ListItem/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Dropdown, MenuItem } from 'react-bootstrap'
 import './style.scss'
 
 export class ListItem extends React.Component {
@@ -14,7 +13,7 @@ export class ListItem extends React.Component {
         )
     }
     render() {
-        const { item, onClick, actions, children, active, className, draggable=false } = this.props
+        const { item, onClick, children, active, className, draggable=false } = this.props
         const classes = [
             'ListItem__list-item',
             'list-item-view',
@@ -27,29 +26,6 @@ export class ListItem extends React.Component {
                     <div className="item-info" onClick={onClick.bind(this, item)}>
                         {children}
                     </div>
-                    {actions &&
-                        <Dropdown className="ListItem__more-actions" id={`dropdownMenuFor${item._id}`}>
-                            <Dropdown.Toggle noCaret className="dropdown__toggle">
-                                <i className="icon-dots-vertical" />
-                            </Dropdown.Toggle>
-                            <Dropdown.Menu className="dropdown dropdown__menu more-activity-menu">
-                                <li>
-                                    <div className="dropdown__menu-label">
-                                        Actions
-                                        <button className="dropdown__menu-close">
-                                            <i className="icon-close-small" />
-                                        </button>
-                                    </div>
-                                </li>
-                                <MenuItem divider />
-                                {actions && actions.map((action, index) => (
-                                    <li key={'action' + index} onClick={action.action}>
-                                        <a>{action.label}</a>
-                                    </li>
-                                ))}
-                            </Dropdown.Menu>
-                        </Dropdown>
-                    }
                 </div>
             </li>
         )
@@ -58,7 +34,6 @@ export class ListItem extends React.Component {
 
 ListItem.propTypes = {
     onClick: React.PropTypes.func.isRequired,
-    actions: React.PropTypes.array,
     item: React.PropTypes.object.isRequired,
     active: React.PropTypes.bool,
     children: React.PropTypes.node.isRequired,
