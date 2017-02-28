4c8abba063db7c185b2fc5d32b059c7c0e2860e3
Edouard Richard <edou4rd@gmail.com>
2017-02-28 17:58:23 +0100

    AdvancedSearchPanelContainer as a function. No need of state

--- client/containers/AdvancedSearchPanelContainer/index.jsx
+++ client/containers/AdvancedSearchPanelContainer/index.jsx
@@ -4,44 +4,33 @@ import { AdvancedSearchForm } from '../../containers/AdvancedSearchForm/index'
 import * as actions from '../../actions'
 import './style.scss'
 
-export class AdvancedSearchPanel extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-
-    onCloseSearch() {
-        this.props.closeAdvancedSearch()
-    }
-
-    render() {
-        const classes = [
-            'Planning__advanced-search',
-            this.props.className,
-        ].join(' ')
-        return (
-            <div className={classes}>
-                <header className="subnav">
-                    <h3 className="subnav__page-title">
-                        <span>Advanced Search</span>
-                    </h3>
-                    <a onClick={this.onCloseSearch.bind(this)} className="close">
-                        <i className="icon-close-small" />
-                    </a>
-                </header>
-                <AdvancedSearchForm />
-            </div>
-        )
-    }
+export function AdvancedSearchPanel({ onCloseAdvancedSearch, className }) {
+    const classes = [
+        'Planning__advanced-search',
+        className,
+    ].join(' ')
+    return (
+        <div className={classes}>
+            <header className="subnav">
+                <h3 className="subnav__page-title">
+                    <span>Advanced Search</span>
+                </h3>
+                <a onClick={onCloseAdvancedSearch} className="close">
+                    <i className="icon-close-small" />
+                </a>
+            </header>
+            <AdvancedSearchForm />
+        </div>
+    )
 }
 
 AdvancedSearchPanel.propTypes = {
     className: React.PropTypes.string,
-    closeAdvancedSearch: React.PropTypes.func.isRequired
+    onCloseAdvancedSearch: React.PropTypes.func.isRequired
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    closeAdvancedSearch: () => (dispatch(actions.closeAdvancedSearch()))
+    onCloseAdvancedSearch: () => (dispatch(actions.closeAdvancedSearch()))
 })
 
 export const AdvancedSearchPanelContainer = connect(
