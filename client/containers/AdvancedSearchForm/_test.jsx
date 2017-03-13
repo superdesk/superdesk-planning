74abba435a4048ac06a39f2a0cfc1d3eda9850e1
Edouard Richard <edou4rd@gmail.com>
2017-03-13 13:16:31 +0100

    Adv search
    Fix location search field
    
    Category field added
    
    Tests added for advanced search

---
+++ client/containers/AdvancedSearchForm/_test.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { AdvancedSearchForm } from '../index'
+import { createTestStore } from '../../utils'
+import { Provider } from 'react-redux'
+import moment from 'moment'
+
+describe('<AdvancedSearchForm />', () => {
+
+    function checkQueryForParameters({params, expectedQuery}) {
+        return new Promise((resolve) => {
+            let store = createTestStore({
+                extraArguments: {
+                    apiQuery: (resource, q) => {
+                        expect(JSON.parse(q.source)).toEqual(expectedQuery)
+                        resolve()
+                    }
+                }
+            })
+            const wrapper = mount(
+                <Provider store={store}>
+                    <AdvancedSearchForm initialValues={params} />
+                </Provider>
+            )
+            wrapper.find('form').simulate('submit')
+        })
+    }
+
+    it('perfrom a search', (done) => {
+        Promise.all([
+            {
+                params: { name: 'a name' },
+                expectedQuery: {
+                    query: { bool: { should: [
+                        { match: { name: 'a name' } },
+                        { match: { definition_short: 'a name' } },
+                    ] } },
+                    filter: {},
+                }
+            },
+            {
+                params: { name: 'a name', anpa_category: [{ qcode: 'cat' }], location: 'paris' },
+                expectedQuery: {
+                    query: { bool: { should: [
+                        { match: { name: 'a name' } },
+                        { match: { definition_short: 'a name' } },
+                        { match: { 'location.name': 'paris' } },
+                        { match: { 'anpa_category.qcode': 'cat' } },
+                    ] } },
+                    filter: {},
+                }
+            },
+            {
+                params: { dates: {
+                    start: moment.utc('2016-01-01'),
+                    end: moment.utc('2017-01-01'),
+                } },
+                expectedQuery: {
+                    query: {},
+                    filter: { range: {
+                        'dates.start': { gte: '2016-01-01T00:00:00.000Z' },
+                        'dates.end': { lte: '2017-01-01T00:00:00.000Z' },
+                    }},
+                }
+            }
+        ].map((test) => (checkQueryForParameters(test))))
+        .then(done)
+    })
+})
