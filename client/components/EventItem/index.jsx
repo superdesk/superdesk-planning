162664f65ae5091bdbcc8c77a5fbc60e2719f95b
vikas <nrvikas@gmail.com>
2017-03-16 21:42:42 +1100

    [SDESK-896] Added time--short class to event item's time field as it can be visible when advanced search pane is opened (#107)
    [SDESK-896] Review changes

--- client/components/EventItem/index.jsx
+++ client/components/EventItem/index.jsx
@@ -21,7 +21,8 @@ export const EventItem = ({ event, onClick }) => {
             <div className="line">
                 <span className="event__title keyword">{event.name}</span>
                 <span className="item-heading">{event.definition_short}</span>
-                <time className="time__short" title={time}>{time}</time>
+                <time className="time--short" title={time}>{time}</time>
+
             </div>
             <div className="line">
                 <span className="container">
