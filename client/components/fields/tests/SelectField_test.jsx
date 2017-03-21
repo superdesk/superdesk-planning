d317c2628371caf30dfc6f54c9515cc755aae80e
Edouard <vied12@gmail.com>
2017-03-21 09:52:44 +0100

    adds tests for OccurStatusField and CategoryField (#117)

---
+++ client/components/fields/tests/SelectField_test.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import { OccurStatusField, CategoryField } from '../index'
+import { createTestStore } from '../../../utils'
+import { Provider } from 'react-redux'
+import { reduxForm, Field } from 'redux-form'
+import * as actions from '../../../actions'
+
+const renderComponentField = (component) => (
+    () => (
+        <Field name="field_name"
+               component={component}/>
+    )
+)
+
+describe('<SelectField />', () => {
+    it('OccurStatusField', () => {
+        const FormComponent = reduxForm({ form: 'form' })(renderComponentField(OccurStatusField))
+        const initialState = {
+            vocabularies: {
+                eventoccurstatus: [{ name: 'occ', qcode: 'qcode' }]
+            }
+        }
+        const store = createTestStore({ initialState })
+        const wrapper = mount(
+            <Provider store={store}>
+                <FormComponent/>
+            </Provider>
+        )
+        wrapper.find('Select').props().onChange({ name: 'lab', value: { name: 'lab' } })
+        expect(wrapper.find('Select').props().value.label).toEqual('lab')
+        expect(wrapper.find('Select').props().options[0].label).toEqual('occ')
+    })
+
+    it('CategoryField', () => {
+        const FormComponent = reduxForm({ form: 'form' })(renderComponentField(CategoryField))
+        const initialState = {
+            vocabularies: {
+                categories: [{ name: 'cat', qcode: 'qcode' }]
+            }
+        }
+        const store = createTestStore({ initialState })
+        const wrapper = mount(
+            <Provider store={store}>
+                <FormComponent/>
+            </Provider>
+        )
+        wrapper.find('Select').props().onChange([{ name: 'lab', value: { name: 'lab' } }])
+        expect(wrapper.find('Select').props().value[0].label).toEqual('lab')
+        expect(wrapper.find('Select').props().options[0].label).toEqual('cat')
+    })
+
+    it('loads categories', (done) => {
+        const store = createTestStore()
+        store.dispatch(actions.loadCVocabularies()).then(() => {
+            expect(store.getState().vocabularies.categories.length).toBe(2)
+            done()
+        })
+    })
+})
