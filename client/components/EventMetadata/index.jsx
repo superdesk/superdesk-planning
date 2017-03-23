79fdbdde7a9702982be45d62749cccf1878a51a5
nrvikas@gmail.com <nrvikas@gmail.com>
2017-03-23 14:45:44 +1100

    [SDESK-939] Event time mismatch in planning view
    [SDESK-939] Format change

--- client/components/EventMetadata/index.jsx
+++ client/components/EventMetadata/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { get, has, some } from 'lodash'
+import { get, has, some, cloneDeep } from 'lodash'
+import moment from 'moment'
 
 function renderDict(event, label, ...keys) {
     if (some(keys, (k) => has(event, k))) {
@@ -10,6 +11,20 @@ function renderDict(event, label, ...keys) {
             ))
         ]
     }
+}
+
+/**
+* Function to perform any UI specific formatting required for rendering the event object
+*/
+function getUIFormattedEvent(event) {
+    if (!event) return
+
+    let formattedEvent = cloneDeep(event)
+    if ( event.dates ) {
+        formattedEvent.dates.start = event.dates.start ? moment(event.dates.start).format('MM/DD/YYYY HH:mm') : null
+        formattedEvent.dates.end = event.dates.end ? moment(event.dates.end).format('MM/DD/YYYY HH:mm') : null
+    }
+    return formattedEvent
 }
 
 const FIELDS = [
@@ -27,7 +42,7 @@ export function EventMetadata ({ event }) {
     return (
         <div className="metadata-view">
             <dl>
-                {FIELDS.map((arrayProps) => renderDict.bind(null, event).apply(null, arrayProps))}
+                {FIELDS.map((arrayProps) => renderDict.bind(null, getUIFormattedEvent(event)).apply(null, arrayProps))}
             </dl>
         </div>
     )
