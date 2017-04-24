7ebcadf4d3e61d35122d4d918f9dd36b4db9b8b4
Mark Pittaway <mark.pittaway@gmail.com>
2017-04-24 15:05:05 +1000

    [SDESK-1157] - Action buttons for coverages should be consistent.

--- client/containers/PlanningForm/_test.jsx
+++ client/containers/PlanningForm/_test.jsx
@@ -4,32 +4,34 @@ import { PlanningForm } from '../index'
 import React from 'react'
 import { Provider } from 'react-redux'
 import sinon from 'sinon'
+import { cloneDeep } from 'lodash'
 
 describe('<PlanningForm />', () => {
-    it('removes a coverage', () => {
-        const initialState = {
-            planning: {
-                plannings: {
-                    2: {
-                        _id: '2',
-                        slugline: 'slug',
-                        coverages: [{ _id: 'coverage1' }],
-                    },
+    const initialState = {
+        planning: {
+            plannings: {
+                2: {
+                    _id: '2',
+                    slugline: 'slug',
+                    coverages: [{ _id: 'coverage1' }],
                 },
-                agendas: [{
-                    _id: '1',
-                    name: 'agenda',
-                }],
-                currentAgendaId: '1',
-                currentPlanningId: '2',
             },
-        }
+            agendas: [{
+                _id: '1',
+                name: 'agenda',
+            }],
+            currentAgendaId: '1',
+            currentPlanningId: '2',
+        },
+    }
+
+    it('removes a coverage', () => {
         const spyRemove = sinon.spy((resource, item) => {
             expect(item._id).toBe('coverage1')
             expect(spyRemove.callCount).toBe(1)
         })
         const store = createTestStore({
-            initialState,
+            initialState: cloneDeep(initialState),
             extraArguments: { apiRemove: spyRemove },
         })
         const wrapper = mount(
@@ -42,5 +44,35 @@ describe('<PlanningForm />', () => {
         expect(wrapper.mount().find('CoveragesFieldArray').find('.Coverage__item').length).toBe(0)
         wrapper.find('form').simulate('submit')
         expect(wrapper.mount().find('CoveragesFieldArray').find('.Coverage__item').length).toBe(0)
+    })
+
+    it('cancel resets the form', () => {
+        const store = createTestStore({ initialState: cloneDeep(initialState) })
+
+        const wrapper = mount(
+            <Provider store={store}>
+                <PlanningForm />
+            </Provider>
+        )
+
+        const saveButton = wrapper.find('footer button[type="submit"]').first()
+        const cancelButton = wrapper.find('footer button[type="button"]').first()
+        const sluglineInput = wrapper.find('Field [name="slugline"]')
+
+        // Save/Cancel buttons start out as disabled
+        expect(saveButton.props().disabled).toBe(true)
+        expect(cancelButton.props().disabled).toBe(true)
+
+        // Modify the slugline and ensure the save/cancel buttons are active
+        sluglineInput.simulate('change', { target: { value: 'NewSlug' } })
+        expect(sluglineInput.props().value).toBe('NewSlug')
+        expect(saveButton.props().disabled).toBe(false)
+        expect(cancelButton.props().disabled).toBe(false)
+
+        // Cancel the modifications and ensure the save/cancel buttons are disabled once again
+        cancelButton.simulate('click')
+        expect(sluglineInput.props().value).toBe('slug')
+        expect(saveButton.props().disabled).toBe(true)
+        expect(cancelButton.props().disabled).toBe(true)
     })
 })
