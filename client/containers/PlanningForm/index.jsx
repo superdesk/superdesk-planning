d2fb469127f2123f067042ff4000c9b74e6bcb87
Mark Pittaway <mark.pittaway@gmail.com>
2017-04-24 15:05:05 +1000

    [SDESK-1157] - Action buttons for coverages should be consistent.

--- client/containers/PlanningForm/index.jsx
+++ client/containers/PlanningForm/index.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { Field, FieldArray, reduxForm, propTypes } from 'redux-form'
 import * as actions from '../../actions'
 import * as selectors from '../../selectors'
+import './style.scss'
 
 class Component extends React.Component {
 
@@ -12,31 +13,41 @@ class Component extends React.Component {
     }
 
     render() {
-        const { handleSubmit, pristine, submitting } = this.props
+        const { handleSubmit, pristine, submitting, reset } = this.props
         return (
             <form onSubmit={handleSubmit} className="PlanningForm">
-                <fieldset>
-                    <Field
-                        name="slugline"
-                        component={fields.InputField}
-                        type="text"
-                        label="Slugline"/>
-                    <Field
-                        name="headline"
-                        component={fields.InputField}
-                        type="text"
-                        label="Headline"/>
-                    <Field
-                        name="anpa_category"
-                        component={fields.CategoryField}
-                        label="Category"/>
-                </fieldset>
-                <h3>Coverages</h3>
-                <FieldArray name="coverages" component={fields.CoveragesFieldArray} />
-                <button
-                    className="btn btn-default"
-                    type="submit"
-                    disabled={pristine || submitting}>Submit</button>
+                <div>
+                    <fieldset>
+                        <Field
+                            name="slugline"
+                            component={fields.InputField}
+                            type="text"
+                            label="Slugline"/>
+                        <Field
+                            name="headline"
+                            component={fields.InputField}
+                            type="text"
+                            label="Headline"/>
+                        <Field
+                            name="anpa_category"
+                            component={fields.CategoryField}
+                            label="Category"/>
+                    </fieldset>
+                    <h3>Coverages</h3>
+                    <FieldArray name="coverages" component={fields.CoveragesFieldArray} />
+                </div>
+
+                <footer>
+                    <button
+                        className="btn btn--primary"
+                        type="submit"
+                        disabled={pristine || submitting}>Save</button>
+                    <button
+                        className="btn"
+                        type="button"
+                        disabled={pristine || submitting} onClick={reset}>Cancel</button>
+                </footer>
+
             </form>
         )
     }
