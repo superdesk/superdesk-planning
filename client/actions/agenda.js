04fa602e85daad9881d70d7909110267715099d3
Mark Lewis <thnkloud9@gmail.com>
2017-05-16 16:03:27 +0200

    fix(client): SDESK-1297: After adding an event to an agenda, the event cannot be deleted from the events list

--- client/actions/agenda.js
+++ client/actions/agenda.js
@@ -3,6 +3,7 @@ import * as selectors from '../selectors'
 import { SubmissionError } from 'redux-form'
 import { cloneDeep, get } from 'lodash'
 import { closePlanningEditor, fetchPlannings, savePlanning } from './planning'
+import { fetchEvents } from './events'
 import { PRIVILEGES } from '../constants'
 import { checkPermission } from '../utils'
 
@@ -136,6 +137,8 @@ const _addPlanningToAgenda = ({ planning, agenda }) => (
         .then((agenda) => {
             // replace the agenda in the store
             dispatch(addOrReplaceAgenda(agenda))
+            // reload event list as event expire will have changed to null
+            dispatch(fetchEvents())
             return agenda
         })
     }
