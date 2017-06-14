ffb1560963a5021323b9d839024394d47612ac50
Mark Lewis <thnkloud9@gmail.com>
2017-06-14 14:26:37 +0200

    fix(client): SDESK-1342: Events list: show "today" as the first day in the list

--- client/selectors/index.js
+++ client/selectors/index.js
@@ -6,6 +6,8 @@ import { ITEM_STATE } from '../constants'
 
 export const getAgendas = (state) => state.agenda.agendas
 export const getCurrentPlanningId = (state) => state.planning.currentPlanningId
+export const getCurrentSearch = (state) => state.events.search.currentSearch ?
+    state.events.search.currentSearch : {}
 export const getEvents = (state) => state.events.events
 export const isEventListShown = (state) =>state.events.show
 export const getPreviousEventRequestParams = (state) => get(state.events, 'lastRequestParams', {})
@@ -170,15 +172,26 @@ export const getEventsWithMoreInfo = createSelector(
 * the associated events.
 */
 export const getEventsOrderedByDay = createSelector(
-    [getEventsWithMoreInfo],
-    (events) => {
+    [getCurrentSearch, getEventsWithMoreInfo],
+    (currentSearch, events) => {
         if (!events) return []
+        // check if search exists
+        let search = (currentSearch.advancedSearch || currentSearch.fulltext) ? true : false
         // order by date
         events = events.sort((a, b) => a.dates.start - b.dates.start)
         var days = {}
         function addEventToDate(event, date) {
             date = date || event.dates.start
             date = date.format('YYYY-MM-DD')
+            // if not in search mode, only add dates in the future
+            if (!search) {
+                let newDate = moment(date)
+                let now = moment()
+                if (!moment(newDate).isSameOrAfter(now)) {
+                    return false
+                }
+            }
+
             if (!days[date]) {
                 days[date] = []
             }
