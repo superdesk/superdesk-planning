a27a8cd6b5e7c67ce1fb30bcdf3b0939aeb5b85b
Mayur Dhamanwala <mdhamanwala@aap.com.au>
2017-10-09 12:30:11 +1100

    [SDESK-1998] Create content from assignment.

---
+++ server/planning/assignments_content.py
@@ -0,0 +1,128 @@
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+"""Creates content based on the assignment"""
+
+import superdesk
+from eve.utils import config
+from apps.archive.common import insert_into_versions
+from apps.auth import get_user_id
+from apps.templates.content_templates import get_item_from_template
+from planning.planning_export import get_desk_template
+from superdesk.errors import SuperdeskApiError
+from .common import ASSIGNMENT_WORKFLOW_STATE
+
+
+def get_item_from_assignment(assignment):
+    """Get the item from assignment
+
+    :param dict assignment: Assignment document
+    :return dict: item
+    """
+    item = {}
+    if not assignment:
+        return item
+
+    desk_id = assignment.get('assigned_to').get('desk')
+    desk = superdesk.get_resource_service('desks').find_one(req=None, _id=desk_id)
+    template = get_desk_template(desk)
+    item = get_item_from_template(template)
+
+    slugline = (assignment.get('planning') or {}).get('slugline')
+
+    if slugline:
+        item['slugline'] = slugline
+
+    ednote = (assignment.get('planning') or {}).get('ednote')
+
+    if ednote:
+        item['ednote'] = ednote
+
+    item['task'] = {
+        'desk': desk['_id'],
+        'user': get_user_id(),
+        'stage': desk['working_stage'],
+    }
+
+    return item
+
+
+class AssignmentsContentService(superdesk.Service):
+
+    def on_create(self, docs):
+        for doc in docs:
+            self._validate(doc)
+
+    def create(self, docs):
+        ids = []
+        production = superdesk.get_resource_service('archive')
+        assignments_service = superdesk.get_resource_service('assignments')
+        for doc in docs:
+            assignment = assignments_service.find_one(req=None, _id=doc.pop('assignment_id'))
+            item = get_item_from_assignment(assignment)
+            item[config.VERSION] = 1
+            item.setdefault('type', 'text')
+            item.setdefault('slugline', 'Planning')
+            item['assignment_id'] = assignment[config.ID_FIELD]
+
+            # create content
+            ids = production.post([item])
+            insert_into_versions(doc=item)
+
+            # create delivery references
+            superdesk.get_resource_service('delivery').post([{
+                'item_id': item[config.ID_FIELD],
+                'assignment_id': assignment[config.ID_FIELD],
+                'planning_id': assignment['planning_item'],
+                'coverage_id': assignment['coverage_item']
+            }])
+
+            # set the assignment to in progress
+            assignments_service.patch(assignment[config.ID_FIELD],
+                                      {
+                                          'assigned_to': {
+                                              'user': str(item.get('task').get('desk')),
+                                              'desk': str(item.get('task').get('desk')),
+                                              'state': ASSIGNMENT_WORKFLOW_STATE.IN_PROGRESS
+                                          }}
+                                      )
+            doc.update(item)
+            ids.append(doc['_id'])
+        return ids
+
+    def _validate(self, doc):
+        """Validate the doc for content creation"""
+        assignment = superdesk.get_resource_service('assignments').find_one(req=None,
+                                                                            _id=doc.get('assignment_id'))
+        if not assignment:
+            raise SuperdeskApiError.badRequestError('Assignment not found.')
+
+        if assignment.get('assigned_to').get('state') != ASSIGNMENT_WORKFLOW_STATE.ASSIGNED:
+            raise SuperdeskApiError.badRequestError('Assignment workflow started. Cannot create content.')
+
+        delivery = superdesk.get_resource_service('delivery').find_one(req=None,
+                                                                       assignment=assignment.get(config.ID_FIELD))
+        if delivery:
+            raise SuperdeskApiError.badRequestError('Content already exists for the assignment. '
+                                                    'Cannot create content.')
+
+
+class AssignmentsContentResource(superdesk.Resource):
+    endpoint_name = 'assignments_content'
+    resource_title = endpoint_name
+    url = 'assignments/content'
+    schema = {
+        'assignment_id': {
+            'type': 'string',
+            'required': True
+        }
+    }
+    resource_methods = ['POST']
+    item_methods = []
+
+    privileges = {'POST': 'archive'}
---
+++ server/planning/delivery.py
@@ -0,0 +1,52 @@
+# This file is part of Superdesk.
+#
+# Copyright 2013, 2014 Sourcefabric z.u. and contributors.
+#
+# For the full copyright and license information, please see the
+# AUTHORS and LICENSE files distributed with this source code, or
+# at https://www.sourcefabric.org/superdesk/license
+
+"""Delivery internal collection to stores delivery references"""
+
+import superdesk
+import logging
+
+logger = logging.getLogger(__name__)
+
+delivery_schema = {
+    # planning item id
+    'planning_id': {
+        'type': 'string'
+    },
+
+    # coverage item id
+    'coverage_id': {
+        'type': 'string'
+    },
+
+    # assignment item id
+    'assignment_id': {
+        'type': 'ObjectId'
+    },
+
+    # item_id
+    'item_id': {
+        'type': 'string'
+    }
+}
+
+
+class DeliveryResource(superdesk.Resource):
+    url = 'delivery'
+    endpoint_name = url
+    schema = delivery_schema
+
+    internal_resource = True
+    resource_methods = []
+    item_methods = []
+    mongo_indexes = {
+        'planning_id_1': ([('planning_id', 1)], {'background': True}),
+        'assignment_id_1': ([('assignment_id', 1)], {'background': True}),
+        'item_id_1': ([('item_id', 1)], {'background': True})
+    }
+    query_objectid_as_string = True
