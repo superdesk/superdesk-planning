951a3a1c8e4b3f46f59ceb231fb90438da79de3c
Petr Jasek <jasekpetr@gmail.com>
2017-11-22 10:17:35 +0100

    close dropdown on click outside

--- client/components/SubnavDropdown.jsx
+++ client/components/SubnavDropdown.jsx
@@ -1,24 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { defer } from 'lodash';
 
 class SubnavDropdown extends React.Component {
     constructor(props) {
         super(props);
         this.state = {open: false};
         this.toggle = this.toggle.bind(this);
+        this.close = this.close.bind(this);
     }
 
     toggle() {
-        this.setState({open: !this.state.open});
+        // change state only when click event handling is over
+        this.inToggle = true;
+        defer(() => {
+            this.setState({open: !this.state.open});
+            this.inToggle = false;
+        });
+    }
+
+    close() {
+        if (!this.inToggle && this.state.open) {
+            this.setState({open: false});
+        }
+    }
+
+    componentDidMount() {
+        document.addEventListener('click', this.close);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.close);
     }
 
     render() {
+        const isCreate = this.props.icon === 'icon-plus-large';
         const className = classNames('dropdown dropdown--align-right', {open: this.state.open});
+        const buttonClassName = classNames('dropdown__toggle navbtn', {
+            'sd-create-btn': isCreate,
+        });
+
         return (
             <div className={className}>
-                <button className="dropdown__toggle navbtn dropdown-toggle" onClick={this.toggle}>
+                <button
+                    ref={(btn) => this.btn = btn}
+                    className={buttonClassName}
+                    onClick={this.toggle}>
                     <i className={this.props.icon} />
+                    {isCreate && (
+                        <span className="circle" />
+                    )}
                 </button>
                 {this.state.open && (
                     <ul className="dropdown__menu dropdown--align-right">
