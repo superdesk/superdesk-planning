c3845f0fcb700c0f36101d21e8d760920962bee1
nrvikas@gmail.com <nrvikas@gmail.com>
2018-02-21 17:10:10 +1100

    [SDESK-2469] Inherit internal_note, ednote from planning item to new coverages

--- client/constants/coverages.js
+++ client/constants/coverages.js
@@ -2,7 +2,11 @@ import {get} from 'lodash';
 
 export const COVERAGES = {
     DEFAULT_VALUE: (newsCoverageStatus, planningItem) => ({
-        planning: {slugline: get(planningItem, 'slugline')},
+        planning: {
+            slugline: get(planningItem, 'slugline'),
+            internal_note: get(planningItem, 'internal_note'),
+            ednote: get(planningItem, 'ednote'),
+        },
         news_coverage_status: newsCoverageStatus[0]
     })
 };
