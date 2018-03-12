0f9b1de4cc6af39341c0783bab6e99b59a89bb5d
nrvikas@gmail.com <nrvikas@gmail.com>
2018-03-12 15:16:46 +1100

    [SDESK-2529] False cotent-link error when creating coverages

--- client/actions/planning/ui.js
+++ client/actions/planning/ui.js
@@ -517,9 +517,7 @@ const unpublish = (item) => (
 
 const save = (item) => (
     (dispatch, getState) => {
-        const modalType = selectors.getCurrentModalType(getState());
-
-        if (modalType === MODALS.ADD_TO_PLANNING) {
+        if (selectors.general.currentWorkspace(getState()) === WORKSPACE.AUTHORING ) {
             return dispatch(self.saveFromAuthoring(item));
         } else {
             return dispatch(self.saveAndReloadCurrentAgenda(item));
