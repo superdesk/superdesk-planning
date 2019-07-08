4d39e561efb896bdee5efa86e2af47becae0ffaa
vikas <nrvikas@gmail.com>
2019-07-09 09:22:27 +1000

    [SDESK-4440][SDESK-4449] Hide edit icon and auto-open added coverage in add-to-planning modal (#1247)

--- client/components/Main/ItemPreview/PreviewContentTab.jsx
+++ client/components/Main/ItemPreview/PreviewContentTab.jsx
@@ -5,14 +5,18 @@ import {ITEM_TYPE} from '../../../constants';
 import {EventPreviewContent} from '../../Events/EventPreviewContent';
 import {PlanningPreviewContent} from '../../Planning/PlanningPreviewContent';
 
-export const PreviewContentTab = ({item, hideRelatedItems}) => {
+export const PreviewContentTab = ({item, hideRelatedItems, hideEditIcon}) => {
     const itemType = getItemType(item);
 
     switch (itemType) {
     case ITEM_TYPE.EVENT:
-        return (<EventPreviewContent hideRelatedItems={hideRelatedItems} />);
+        return (<EventPreviewContent
+            hideRelatedItems={hideRelatedItems}
+            hideEditIcon={hideEditIcon} />);
     case ITEM_TYPE.PLANNING:
-        return (<PlanningPreviewContent hideRelatedItems={hideRelatedItems} />);
+        return (<PlanningPreviewContent
+            hideRelatedItems={hideRelatedItems}
+            hideEditIcon={hideEditIcon} />);
     default:
         return null;
     }
@@ -21,6 +25,7 @@ export const PreviewContentTab = ({item, hideRelatedItems}) => {
 PreviewContentTab.propTypes = {
     item: PropTypes.object.isRequired,
     hideRelatedItems: PropTypes.bool,
+    hideEditIcon: PropTypes.bool,
 };
 
 PreviewContentTab.defaultProps = {hideRelatedItems: false};
