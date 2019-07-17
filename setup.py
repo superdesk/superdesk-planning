35a0328d8fc47142bce2af6b2bcce001e9ef78b6
Mark Pittaway <mark.pittaway@gmail.com>
2019-07-17 11:29:17 +1000

    Release 1.6.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ package_data = {
 
 setup(
     name="superdesk-planning",
-    version="1.6.0",
+    version="1.6.1",
     package_dir={'': 'server'},
     packages=find_packages('server'),
     package_data=package_data,
