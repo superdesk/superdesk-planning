3c91df5fe1ec27997bb9a6b063c8cb09d2a765db
Tomas Kikutis <t.kikutis@gmail.com>
2019-07-18 14:16:45 +0200

    add typings to modal body

--- client/components/Modal/Body.tsx
+++ client/components/Modal/Body.tsx
@@ -3,19 +3,32 @@ import PropTypes from 'prop-types';
 import {Modal as _Modal} from 'react-bootstrap';
 import classNames from 'classnames';
 
-export default function Body({children, noPadding, fullHeight, noScroll}) {
-    return (
-        <_Modal.Body
-            className={classNames(
-                'modal__body',
-                {
-                    'modal__body--no-padding': noPadding,
-                    'modal__body--full-height': fullHeight,
-                    'modal__body--no-scroll': noScroll,
-                }
-            )}>
-            {children}
-        </_Modal.Body>);
+interface IProps {
+    children?: any;
+    noPadding?: boolean;
+    fullHeight?: boolean;
+    noScroll?: boolean;
+}
+
+export default class Body extends React.Component<IProps> {
+    static propTypes: any;
+
+    render() {
+        const {children, noPadding, fullHeight, noScroll} = this.props;
+
+        return (
+            <_Modal.Body
+                className={classNames(
+                    'modal__body',
+                    {
+                        'modal__body--no-padding': noPadding,
+                        'modal__body--full-height': fullHeight,
+                        'modal__body--no-scroll': noScroll,
+                    }
+                )}>
+                {children}
+            </_Modal.Body>);
+    }
 }
 
 Body.propTypes = {
