72628f673c14ba770f681e0998f6ffa06d5e3cd2
Tomas Kikutis <t.kikutis@gmail.com>
2019-07-18 12:05:19 +0200

    use superdesk api

--- client/components/Events/ManageEventTemplatesModal.jsx
+++ client/components/Events/ManageEventTemplatesModal.jsx
@@ -3,126 +3,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {Modal} from '../../components';
-import {ColumnBox} from '../../components/UI';
-import {List} from '../../components/UI';
-import {Form} from '../../components/UI';
-import {TOOLTIPS} from '../../constants';
-import {connectServices} from 'superdesk-core/scripts/core/helpers/ReactRenderAsync';
-import {cloneDeep} from 'lodash';
 import eventsApi from '../../actions/events/api';
 
-class EventTemplateEdit extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = cloneDeep(this.props.template);
-
-        this.handleFieldChange = this.handleFieldChange.bind(this);
-    }
-    handleFieldChange(field, nextValue) {
-        this.setState({
-            [field]: nextValue,
-        });
-    }
-    render() {
-        return (
-            <div style={{padding: 20}}>
-                <Form.Row>
-                    <Form.TextInput
-                        field="template_name"
-                        label={gettext('Template name')}
-                        required={true}
-                        value={this.state.template_name}
-                        onChange={this.handleFieldChange}
-                        invalid={false}
-                        message={null}
-                        autoFocus={true}
-                    />
-                </Form.Row>
-
-                <div style={{display: 'flex', justifyContent: 'flex-end'}}>
-                    <button
-                        onClick={this.props.onEditCancel}
-                        className="btn"
-                    >
-                        {gettext('Cancel')}
-                    </button>
-
-                    <button
-                        onClick={() => this.props.onEditComplete(this.state)}
-                        disabled={
-                            this.state.template_name.trim().length < 1
-                            || this.props.template.template_name === this.state.template_name}
-                        className="btn btn--primary"
-                    >
-                        {gettext('Save')}
-                    </button>
-                </div>
-            </div>
-        );
-    }
-}
-
-EventTemplateEdit.propTypes = {
-    template: PropTypes.object,
-    onEditComplete: PropTypes.func,
-    onEditCancel: PropTypes.func,
-};
-
-class EventTemplatesList extends React.Component {
-    render() {
-        return (
-            <ColumnBox.Box>
-                <ColumnBox.MainColumn padded={true} verticalScroll={true} fullHeight>
-                    {
-                        this.props.eventTemplates.length < 1
-                            ? gettext('There are no saved templates')
-                            : this.props.eventTemplates.map((eventTemplate, i) => (
-                                <List.Item shadow={1} key={i}>
-                                    <List.Column grow={true} border={false}>
-                                        <List.Row>
-                                            <span
-                                                className="sd-overflow-ellipsis sd-list-item--element-grow"
-                                            >
-                                                {eventTemplate.template_name}
-                                            </span>
-                                        </List.Row>
-                                    </List.Column>
-                                    <List.Column border={false}>
-                                        <List.Row>
-                                            <button
-                                                onClick={() => this.props.editTemplate(eventTemplate._id)}
-                                                className="dropdown__toggle"
-                                                title={TOOLTIPS.edit}
-                                                data-flow="left"
-                                            >
-                                                <i className="icon-pencil"/>
-                                            </button>
-
-                                            <button
-                                                onClick={() => this.props.deleteTemplate(eventTemplate._id)}
-                                                className="dropdown__toggle"
-                                                title={TOOLTIPS.delete}
-                                                data-flow="left"
-                                            >
-                                                <i className="icon-trash"/>
-                                            </button>
-                                        </List.Row>
-                                    </List.Column>
-                                </List.Item>
-                            ))
-                    }
-                </ColumnBox.MainColumn>
-            </ColumnBox.Box>
-        );
-    }
-}
-
-EventTemplatesList.propTypes = {
-    eventTemplates: PropTypes.array,
-    editTemplate: PropTypes.func,
-    deleteTemplate: PropTypes.func,
-};
+import {superdeskApi} from '../../superdeskApi';
 
 class ManageEventTemplatesModalComponent extends React.Component {
     constructor(props) {
@@ -161,7 +44,7 @@ class ManageEventTemplatesModalComponent extends React.Component {
     }
 
     deleteTemplate(id) {
-        this.props.modal.confirm(gettext('Confirm delete')).then(() => {
+        superdeskApi.ui.confirm(gettext('Confirm delete')).then(() => {
             this.props.dispatch(eventsApi.removeEventTemplate(
                 this.props.eventTemplates.find(({_id}) => _id === id)
             ));
@@ -171,8 +54,56 @@ class ManageEventTemplatesModalComponent extends React.Component {
     render() {
         const {handleHide} = this.props;
 
+        const {getGenericListPageComponent, ListItemColumn, ListItem} = superdeskApi.components;
+        const {getFormFieldPreviewComponent, FormFieldType} = superdeskApi.forms;
+
+        const EventTemplatesComponent = getGenericListPageComponent('events_template');
+
+        const nameField = {
+            label: gettext('Template name'),
+            type: FormFieldType.textSingleLine,
+            field: 'template_name',
+            required: true,
+        };
+
+        const formConfig = {
+            direction: 'vertical',
+            type: 'inline',
+            form: [
+                nameField,
+            ],
+        };
+
+        const renderRow = (
+            key,
+            item,
+            page
+        ) => (
+            <ListItem
+                key={key}
+            >
+                <ListItemColumn ellipsisAndGrow noBorder>
+                    {getFormFieldPreviewComponent(item, nameField)}
+                </ListItemColumn>
+                <ListItemColumn noBorder>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                    }}>
+                        <button onClick={() => page.startEditing(item._id)}>
+                            <i className="icon-pencil" />
+                        </button>
+                        <button onClick={() => page.deleteItem(item)}>
+                            <i className="icon-trash" />
+                        </button>
+                    </div>
+                </ListItemColumn>
+            </ListItem>
+        );
+
         return (
-            <Modal xLarge={false} show={true} onHide={handleHide}>
+            <Modal xLarge={true} show={true} onHide={handleHide}>
                 <Modal.Header>
                     <a className="close" onClick={handleHide}>
                         <i className="icon-close-small" />
@@ -180,25 +111,14 @@ class ManageEventTemplatesModalComponent extends React.Component {
                     <h3 className="modal__heading">{gettext('Manage Event Templates')}</h3>
                 </Modal.Header>
                 <Modal.Body noPadding={true}>
-                    {
-                        this.state.templateInEditMode == null
-                            ? (
-                                <EventTemplatesList
-                                    eventTemplates={this.props.eventTemplates}
-                                    editTemplate={this.editTemplate}
-                                    deleteTemplate={this.deleteTemplate}
-                                />
-                            )
-                            : (
-                                <EventTemplateEdit
-                                    template={this.props.eventTemplates.find(
-                                        ({_id}) => _id === this.state.templateInEditMode)
-                                    }
-                                    onEditComplete={this.onEditComplete}
-                                    onEditCancel={this.onEditCancel}
-                                />
-                            )
-                    }
+                    <EventTemplatesComponent
+                        renderRow={renderRow}
+                        formConfig={formConfig}
+                        defaultSortOption={{field: nameField.field, direction: 'ascending'}}
+                        fieldForSearch={nameField}
+                        disallowCreatingNewItem
+                        disallowFiltering
+                    />
                 </Modal.Body>
             </Modal>
         );
@@ -208,7 +128,6 @@ class ManageEventTemplatesModalComponent extends React.Component {
 ManageEventTemplatesModalComponent.propTypes = {
     handleHide: PropTypes.func,
     eventTemplates: PropTypes.array,
-    modal: PropTypes.object,
     dispatch: PropTypes.func,
 };
 
@@ -218,8 +137,5 @@ function mapStateToProps(state) {
     };
 }
 
-export const ManageEventTemplatesModal = connect(mapStateToProps)(connectServices(
-    ManageEventTemplatesModalComponent,
-    ['modal']
-));
+export const ManageEventTemplatesModal = connect(mapStateToProps)(ManageEventTemplatesModalComponent);
 
---
+++ client/superdeskApi.js
@@ -0,0 +1,3 @@
+// will be set asynchronously on planning module start
+// members can't be accessed in root module scope synchronously
+export const superdeskApi = {};
