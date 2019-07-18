8bef719ad273f08878f4153a43a3a4a1cbfc56d9
Tomas Kikutis <t.kikutis@gmail.com>
2019-07-18 13:03:59 +0200

    fix type errors for modal

--- client/components/Modal/index.tsx
+++ client/components/Modal/index.tsx
@@ -8,37 +8,60 @@ import {default as Footer} from './Footer';
 import classNames from 'classnames';
 import './style.scss';
 
-export default function Modal({
-    show,
-    handleHide,
-    children,
-    xLarge,
-    large,
-    fill,
-    fullscreen,
-    white,
-    className,
-    fullheight,
-}) {
-    const classes = classNames(className, {
-        modal: true,
-        'modal--large': large,
-        'modal--fill': fill,
-        'modal--fullscreen': fullscreen,
-        'modal--white': white,
-        'modal--x-large': xLarge,
-    });
+interface IProps {
+    show?: any;
+    handleHide?: any;
+    children?: any;
+    xLarge?: any;
+    large?: any;
+    fill?: any;
+    fullscreen?: any;
+    white?: any;
+    className?: any;
+    fullheight?: any;
+}
 
-    return (
-        <_Modal
-            show={show}
-            backdrop={true}
-            className={classes}
-            onHide={handleHide}
-            dialogComponentClass={ModalDialog}
-            dialogClassName={fullheight ? 'modal__dialog--full-height' : undefined}
-        >{children}</_Modal>
-    );
+export class Modal extends React.Component<IProps> {
+    static Header: typeof Header;
+    static Body: typeof Body;
+    static Footer: typeof Footer;
+
+    static propTypes: any;
+
+    render() {
+        const {
+            show,
+            handleHide,
+            children,
+            xLarge,
+            large,
+            fill,
+            fullscreen,
+            white,
+            className,
+            fullheight,
+        } = this.props;
+    
+        const classes = classNames(className, {
+            modal: true,
+            'modal--large': large,
+            'modal--fill': fill,
+            'modal--fullscreen': fullscreen,
+            'modal--white': white,
+            'modal--x-large': xLarge,
+        });
+    
+        return (
+            <_Modal
+                show={show}
+                backdrop={true}
+                className={classes}
+                onHide={handleHide}
+                dialogComponentClass={ModalDialog}
+                dialogClassName={fullheight ? 'modal__dialog--full-height' : undefined}
+            >{children}</_Modal>
+        );
+    }
 }
 
 Modal.propTypes = {
