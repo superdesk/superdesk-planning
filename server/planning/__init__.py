cb2b6467e60dc575b4de0447d4918ca06d6c2da2
Mark Pittaway <mark.pittaway@gmail.com>
2019-08-16 08:24:49 +1000

    Release 1.6.2-rc2

--- server/planning/__init__.py
+++ server/planning/__init__.py
@@ -42,7 +42,7 @@ import planning.feed_parsers  # noqa
 import planning.output_formatters  # noqa
 from planning.planning_download import init_app as init_planning_download_app
 
-__version__ = '1.6.2-rc1'
+__version__ = '1.6.2-rc2'
 
 
 def init_app(app):
