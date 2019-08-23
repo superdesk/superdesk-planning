9da951fbab19fb6efc2cd79249e74af1927f7ada
Petr Jasek <jasekpetr@gmail.com>
2019-08-23 13:40:24 +0200

    test elastic7 on travis

--- server/planning/planning/planning_tests.py
+++ server/planning/planning/planning_tests.py
@@ -35,7 +35,7 @@ class DuplicateCoverageTestCase(TestCase):
                 duplicate_coverage_for_article_rewrite('plan1', 'cov1', {
                     'planning': {
                         'slugline': 'new slugline',
-                        'scheduled': '2029-10-13T15:00.00+0000'
+                        'scheduled': '2029-10-13T15:00:00+0000'
                     },
                     'assigned_to': {
                         'user': '562435231d41c835d7b5fb55',
@@ -50,7 +50,7 @@ class DuplicateCoverageTestCase(TestCase):
             self.assertEqual(len(updated_plan['coverages']), 2)
 
             self.assertEqual(new_coverage['planning']['slugline'], 'new slugline')
-            self.assertEqual(new_coverage['planning']['scheduled'], '2029-10-13T15:00.00+0000')
+            self.assertEqual(new_coverage['planning']['scheduled'], '2029-10-13T15:00:00+0000')
             self.assertEqual(new_coverage['assigned_to']['user'], '562435231d41c835d7b5fb55')
             self.assertEqual(new_coverage['assigned_to']['desk'], 'desk2')
             self.assertEqual(new_coverage['assigned_to']['state'], 'in_progress')
