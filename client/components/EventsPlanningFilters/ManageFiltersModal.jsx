0c757ce8d546e4fb5bd5b4be9dc85901c417bd03
nrvikas@gmail.com <nrvikas@gmail.com>
2019-11-05 11:42:16 +1100

    [SDESK-4741] Scroll issue in the Manage Events & Planning Filters window

--- client/components/EventsPlanningFilters/ManageFiltersModal.jsx
+++ client/components/EventsPlanningFilters/ManageFiltersModal.jsx
@@ -82,8 +82,8 @@ export class ManageFiltersComponent extends React.Component {
                                 {gettext('Add New Filter')}
                             </Button>}
                         </SubNav>}
-                        <ColumnBox.Box>
-                            <ColumnBox.MainColumn padded={true} verticalScroll={true}>
+                        <ColumnBox.Box verticalScroll={true}>
+                            <ColumnBox.MainColumn padded={true}>
                                 <FiltersList
                                     filters={filters}
                                     privileges={privileges}
