853ed3ad49d1609aa3d375a348648b7321c59c3b
Mark Pittaway <mark.pittaway@gmail.com>
2020-01-28 15:57:06 +1100

    * Fix TravisCI install/script phases

--- scripts/install_e2e.sh
+++ scripts/install_e2e.sh
@@ -2,12 +2,12 @@
 
 PLANNING_DIR=`pwd`
 
-cd $PLANNING_DIR/e2e/server
-pip install -r requirements.txt
-
 sudo sed -i 's\enabled: true\enabled: false\' /etc/mongod.conf
 sudo service mongod restart
 sudo service elasticsearch restart
+
+cd $PLANNING_DIR/e2e/server
+pip install -r requirements.txt
 
 cd $PLANNING_DIR/e2e
 npm install --python=python2.7
