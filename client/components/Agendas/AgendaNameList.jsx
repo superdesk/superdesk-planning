23d3f0438989e6d3c34b97ee78fe3d3786f2cd20
nrvikas@gmail.com <nrvikas@gmail.com>
2020-02-28 13:38:15 +1100

    [SDESK-4852] Limit calendar and agenda display in lists

--- client/components/Agendas/AgendaNameList.jsx
+++ client/components/Agendas/AgendaNameList.jsx
@@ -1,24 +1,33 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
 import PropTypes from 'prop-types';
 import {get, sortBy} from 'lodash';
 import {gettext} from '../../utils';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
-export const AgendaNameList = ({agendas}) => (
-    <span>
+export const AgendaNameList = ({agendas}) => {
+    let tooltipElem = (agendas || []).map((a) =>
+        <div key={a._id}>{a.name}{!a.is_enabled ? ' (Disabled)' : ''}</div>);
+
+    const agendaElem = (a, index, arr) => (<OverlayTrigger
+        key={a._id}
+        overlay={<Tooltip id="agenda_tooltip" className="tooltip--text-left">
+            {tooltipElem}
+        </Tooltip>}>
+        {<span
+            className={!a.is_enabled ? 'sd-list-item__text--disabled' : ''}>
+            {a.name}{index === arr.length - 1 ? '' : ', '}
+        </span>}
+    </OverlayTrigger>);
+
+
+    return (<span>
         {get(agendas, 'length', 0) === 0 ? gettext('No Agenda Assigned.') :
             sortBy(agendas.filter((a) => a), [(a) => get(a, 'name', '').toLowerCase()])
-                .map((agenda, index, arr) => {
-                    const className = !agenda.is_enabled ? 'sd-list-item__text--disabled' : '';
-
-                    return (
-                        <span key={index} className={className}>
-                            {agenda.name}{arr.length - 1 > index && ', '}
-                        </span>
-                    );
-                })
+                .map((agenda, index, arr) => agendaElem(agenda, index, arr))
         }
-    </span>
-);
+    </span>);
+};
 
 AgendaNameList.propTypes = {
     agendas: PropTypes.array,
--- client/components/fields/calendars.jsx
+++ client/components/fields/calendars.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+/* eslint-disable react/no-multi-comp */
 
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
 import {get} from 'lodash';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
 export const calendars = ({item, calendars, grow}) => {
     const isCalendarActive = (cal) => (get((calendars || []).find((c) => c.qcode === cal.qcode), 'is_active', false));
+    let tooltipElem = get(item, 'calendars', []).map((c) =>
+        <div key={c.qcode}>{c.name}{!isCalendarActive(c) ? ' (Disabled)' : ''}</div>);
 
-    return (<div className={classNames({'sd-list-item--element-grow sd-overflow-ellipsis': grow})}>
+    const calendarElem = (calendar, index, arr) => (<OverlayTrigger
+        key={calendar.qcode}
+        overlay={<Tooltip id="location_tooltip" className="tooltip--text-left">
+            {tooltipElem}
+        </Tooltip>}>
+        {<span
+            className={!isCalendarActive(calendar) ? 'sd-list-item__text--disabled' : ''}>
+            {calendar.name}{index === arr.length - 1 ? '' : ', '}
+        </span>}
+    </OverlayTrigger>);
+
+    return (<Fragment>
         <span className="sd-list-item__text-label">{gettext('Calendar:')}</span>
         {<span className="sd-overflow-ellipsis sd-list-item__text-strong sd-list-item--element-rm-10">
-            {get(item, 'calendars.length', 0) > 0 && item.calendars.map((c, index, arr) =>
-                <span key={c.qcode}
-                    className={!isCalendarActive(c) ? 'sd-list-item__text--disabled' : ''}>
-                    {c.name}{arr.length - 1 > index && ', '}
-                </span>)
-            }
+            {get(item, 'calendars.length', 0) > 0 && item.calendars.map((c, index, arr) => calendarElem(c, index, arr))}
             {get(item, 'calendars.length', 0) === 0 && <span>{gettext('No  calendar assigned')}</span>}
         </span>}
-    </div>);
+    </Fragment>);
 };
 
 calendars.propTypes = {
