6068c8ac7ed29231ff9bda971229e174d902766e
vikas <nrvikas@gmail.com>
2020-03-02 15:33:18 +1100

    [SDESK-4852] Limit calendar and agenda display in lists (#1444)
    * [SDESK-4852] Limit calendar and agenda display in lists
    
    * changes tooltip to list element

--- client/components/Agendas/AgendaNameList.jsx
+++ client/components/Agendas/AgendaNameList.jsx
@@ -1,24 +1,32 @@
+/* eslint-disable react/no-multi-comp */
 import React from 'react';
 import PropTypes from 'prop-types';
 import {get, sortBy} from 'lodash';
 import {gettext} from '../../utils';
+import {OverlayTrigger, Tooltip} from 'react-bootstrap';
 
-export const AgendaNameList = ({agendas}) => (
-    <span>
+export const AgendaNameList = ({agendas}) => {
+    let tooltipElem = (agendas || []).map((a) =>
+        <div key={a._id}>{a.name}{!a.is_enabled ? ' (Disabled)' : ''}</div>);
+
+    return (<span>
         {get(agendas, 'length', 0) === 0 ? gettext('No Agenda Assigned.') :
-            sortBy(agendas.filter((a) => a), [(a) => get(a, 'name', '').toLowerCase()])
-                .map((agenda, index, arr) => {
-                    const className = !agenda.is_enabled ? 'sd-list-item__text--disabled' : '';
-
-                    return (
-                        <span key={index} className={className}>
-                            {agenda.name}{arr.length - 1 > index && ', '}
-                        </span>
-                    );
-                })
+            (<OverlayTrigger
+                placement="left"
+                overlay={<Tooltip id="agenda_tooltip" className="tooltip--text-left">
+                    {tooltipElem}
+                </Tooltip>}>
+                <span>
+                    {sortBy(agendas.filter((a) => a), [(a) => get(a, 'name', '').toLowerCase()])
+                        .map((a, index, arr) => (<span
+                            key={a._id}
+                            className={!a.is_enabled ? 'sd-list-item__text--disabled' : ''}>
+                            {a.name}{index === arr.length - 1 ? '' : ', '}
+                        </span>))}</span>
+            </OverlayTrigger>)
         }
-    </span>
-);
+    </span>);
+};
 
 AgendaNameList.propTypes = {
     agendas: PropTypes.array,
