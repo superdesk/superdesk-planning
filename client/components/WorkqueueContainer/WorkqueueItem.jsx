5c80f220ff0bcf4ffeedf85c32e83923ee93f084
Nemanja Pavlovic <me@pavlovicnemanja.com>
2020-03-13 13:07:51 +0100

    fix(bottom-bar): Refactored bottom-bar structure

--- client/components/WorkqueueContainer/WorkqueueItem.jsx
+++ client/components/WorkqueueContainer/WorkqueueItem.jsx
@@ -16,20 +16,20 @@ export const WorkqueueItem = ({
     const isActive = getItemId(item) === currentEditId;
 
     return (
-        <li className={isActive ? ' workqueue-item active' : 'workqueue-item'}>
-            <a className="title" onClick={onOpen.bind(null, item)}>
+        <li className={isActive ? 'opened-articles-bar__item opened-articles-bar__item--active' : 'opened-articles-bar__item'}>
+            <a className="opened-articles-bar__item-title" onClick={onOpen.bind(null, item)}>
                 <ItemIcon
                     item={item}
                     color={isActive ? ICON_COLORS.WHITE : ICON_COLORS.BLUE}
                     showRepeating={false}
                 />
-                <span className="item-label">
+                <span>
                     {item.headline || item.slugline || item.name || 'Untitled'}
                     {!isExistingItem(item) && '*'}
                 </span>
             </a>
 
-            <button className="close" onClick={onClose.bind(null, item)}>
+            <button className="opened-articles-bar__item-close" onClick={onClose.bind(null, item)}>
                 <Icon icon="icon-close-small" color={ICON_COLORS.WHITE} />
             </button>
         </li>
