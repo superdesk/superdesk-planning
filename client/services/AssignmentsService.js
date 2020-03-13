e7c0f75747136ca7cc71bf3031d1199305a6ce78
Mark Pittaway <mark.pittaway@gmail.com>
2020-03-13 14:37:15 +1100

    Fix extensions import

--- client/services/AssignmentsService.js
+++ client/services/AssignmentsService.js
@@ -129,7 +129,7 @@ export class AssignmentsService {
                             resolve();
                         });
                 }));
-            })
+            });
 
             iteratePromiseCallbacks(challenges)
                 .then(
