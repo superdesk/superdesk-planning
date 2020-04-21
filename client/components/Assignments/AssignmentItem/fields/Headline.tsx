14c38a7a25ac732023bc842d66edb39ac5c8aecd
pablopunk <pablovarela182@gmail.com>
2020-04-21 13:54:02 +0200

    Headline component

--- client/components/Assignments/AssignmentItem/fields/Headline.tsx
+++ client/components/Assignments/AssignmentItem/fields/Headline.tsx
@@ -3,8 +3,28 @@ import PropTypes from 'prop-types';
 
 interface IProps {
     assignment: any;
+    api: any;
 }
 
-export const HeadlineComponent = ({assignment}: IProps) => (
-    <span>headline</span>
-);
+export const HeadlineComponent = ({assignment, api}: IProps) => {
+    const itemIds = assignment.item_ids;
+
+    if (!itemIds) {
+        return null;
+    }
+
+    const itemId = assignment.item_ids[0];
+
+    if (!itemId) {
+        return null;
+    }
+
+    const [item, setItem] = React.useState(null);
+
+    if (!item) {
+        api.find('archive', itemId).then(setItem);
+        return null;
+    }
+
+    return <span>{item.headline}</span>;
+};
