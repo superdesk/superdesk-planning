ea272e0a1ee3fa75901746bed27af3c556f9c691
Mark Pittaway <mark.pittaway@gmail.com>
2020-05-10 12:18:54 +1000

    e2e: Wait for xhr request to complete for retrieving list of events & planning

--- e2e/cypress/support/common/ui/index.js
+++ e2e/cypress/support/common/ui/index.js
@@ -3,7 +3,14 @@ export function waitForPageLoad() {
     cy.log('UI.waitForPageLoad');
     // Set the timeout waiting for this button to 60 seconds
     // as this can sometimes take a while on initial load
-    cy.get('.icon-plus-large', {timeout: 180000});
+
+    // Wait for the xhr request to complete for retrieving list of events & planning
+    cy.server();
+    cy.route('GET', '**/api/events_planning_search*').as('getPlanningEventsSearch');
+    cy.wait('@getPlanningEventsSearch', {timeout: 180000});
+
+    // Now wait for the plus icon to be present
+    cy.get('.icon-plus-large');
 }
 
 export {ActionMenu} from './actionMenu';
