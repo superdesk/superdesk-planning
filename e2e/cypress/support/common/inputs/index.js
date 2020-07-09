c5916fadd554f8cdc4462c794426977798fbac88
Mark Pittaway <mark.pittaway@gmail.com>
2020-07-09 15:48:03 +1000

    e2e tests

--- e2e/cypress/support/common/inputs/index.js
+++ e2e/cypress/support/common/inputs/index.js
@@ -3,3 +3,4 @@ export {SelectInput} from './selectInput';
 export {SelectMetaTerms} from './selectMetaTerms';
 export {ToggleInput} from './toggleInput';
 export {UrgencyInput} from './urgencyInput';
+export {UserSelectInput} from './userSelectInput';
