55d9c40a95d3259ea4c4dbd26c5a5e18b04ec0f3
Tomas Kikutis <t.kikutis@gmail.com>
2021-01-19 22:16:17 +0100

    Improve translations for CP (#1515)
    * translate group name
    
    * Use gettext from client core. It uses an external library for translation(non angular) which loads translations before loading angular. It then allows translating strings before angular has initializes(needed for registering activities).
    
    * translate labels
    
    * Extract string for translation
    
    * specify correct file extension
    
    * test patching fix
    
    * update client-core version
    
    * Revert "test patching fix"
    
    This reverts commit dcd23d1101dc180fca3c0764c9c6d3df90ff87a0.
    
    * Revert "update client-core version"
    
    This reverts commit ba9263199cb363a34d7f412d55502806cb13c4d7.
    
    * use gettext from superdeskApi

--- client/utils/gettext.js
+++ client/utils/gettext.js
@@ -1,3 +1,5 @@
+import * as utils from 'core/utils';
+
 /**
  * Get translated string
  *
@@ -9,25 +11,7 @@
  * @param {Object} params
  * @return {String}
  */
-export const gettext = (text, params = null) => {
-    if (!text) {
-        return '';
-    }
-
-    const injector = angular.element(document.body).injector();
-
-    if (injector) { // in tests this will be empty
-        return injector.get('gettextCatalog').getString(text, params || {});
-    }
-
-    if (text && params) {
-        const template = (tpl, args) => tpl.replace(/\{{\s*(\w+)\s*}}/g, (_, v) => args[v]);
-
-        return template(text, params);
-    }
-
-    return text;
-};
+export const gettext = (text, params = null) => utils.gettext(text, params);
 
 export function gettextCatalog(text, params = null) {
     if (!text) {
