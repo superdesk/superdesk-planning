1a9bc2f8b5798e46d93a822d559bd0c858ac59fa
Mark Pittaway <mark.pittaway@gmail.com>
2021-02-12 16:42:05 +1100

    Fix generating seach params

--- client/utils/search.ts
+++ client/utils/search.ts
@@ -1,191 +1,153 @@
+import moment from 'moment';
 
-import {ICombinedSearchParams, IEventSearchParams, IPlanningSearchParams, ISearchParams} from '../interfaces';
+import {
+    ICombinedSearchParams,
+    IEventSearchParams,
+    IPlanningSearchParams,
+    ISearchParams,
+    ICommonSearchParams,
+    IEventOrPlanningItem,
+} from '../interfaces';
 import {MAIN} from '../constants';
 
-export function planningParamsToSearchParams(params: IPlanningSearchParams): ISearchParams {
+function commonParamsToSearchParams(params: ICommonSearchParams<IEventOrPlanningItem>): ISearchParams {
     return {
-        name: params.advancedSearch?.name,
-        tz_offset: params.timezoneOffset,
+        item_ids: params.itemIds,
         full_text: params.fulltext,
-        anpa_category: params.advancedSearch?.anpa_category,
-        subject: params.advancedSearch?.subject,
-        posted: params.advancedSearch?.posted,
-        place: params.advancedSearch?.place,
-        language: params.advancedSearch?.language,
-        state: params.advancedSearch?.state,
+        include_killed: params.includeKilled,
+        max_results: params.maxResults,
+        page: params.page,
+        only_future: params.onlyFuture,
+        start_of_week: params.startOfWeek,
         spike_state: params.spikeState,
+        filter_id: params.filter_id,
+        lock_state: params.lock_state,
+        tz_offset: params.timezoneOffset,
+        anpa_category: params.advancedSearch?.anpa_category,
         date_filter: params.advancedSearch?.dates?.range,
         start_date: params.advancedSearch?.dates?.start,
         end_date: params.advancedSearch?.dates?.end,
-        start_of_week: params.startOfWeek,
+        name: params.advancedSearch?.name,
+        place: params.advancedSearch?.place,
+        posted: params.advancedSearch?.posted,
         slugline: params.advancedSearch?.slugline,
-        agendas: params.agendas,
-        no_agenda_assigned: params.noAgendaAssigned,
-        ad_hoc_planning: params.adHocPlanning,
-        exclude_rescheduled_and_cancelled: params.excludeRescheduledAndCancelled,
-        no_coverage: params.advancedSearch?.noCoverage,
-        urgency: params.advancedSearch?.urgency,
-        g2_content_type: params.advancedSearch?.g2_content_type,
-        featured: params.advancedSearch?.featured,
-        include_killed: params.includeKilled,
-        include_scheduled_updates: params.includeScheduledUpdates,
-        max_results: params.maxResults,
-        page: params.page,
+        state: params.advancedSearch?.state,
+        subject: params.advancedSearch?.subject,
+        language: params.advancedSearch?.language,
     };
 }
 
-export function searchParamsToPlanningParams(params: ISearchParams): IPlanningSearchParams {
+function searchParamsToCommonParams(params: ISearchParams): ICommonSearchParams<IEventOrPlanningItem> {
     return {
-        adHocPlanning: params.ad_hoc_planning,
-        agendas: params.agendas,
-        excludeRescheduledAndCancelled: params.exclude_rescheduled_and_cancelled,
-        featured: params.featured,
+        itemIds: params.item_ids,
         fulltext: params.full_text,
         includeKilled: params.include_killed,
-        includeScheduledUpdates: params.include_scheduled_updates,
-        noAgendaAssigned: params.no_agenda_assigned,
-        spikeState: params.spike_state,
+        maxResults: params.max_results,
+        page: params.page,
+        onlyFuture: params.only_future,
         startOfWeek: params.start_of_week,
+        spikeState: params.spike_state,
+        filter_id: params.filter_id,
+        lock_state: params.lock_state,
         timezoneOffset: params.tz_offset,
         advancedSearch: {
             anpa_category: params.anpa_category,
             dates: {
                 range: params.date_filter,
-                start: params.start_date,
-                end: params.end_date,
+                start: params.start_date != undefined ? moment(params.start_date) : undefined,
+                end: params.end_date != undefined ? moment(params.end_date) : undefined,
             },
-            featured: params.featured,
-            g2_content_type: params.g2_content_type,
-            noCoverage: params.no_coverage,
+            name: params.name,
             place: params.place,
             posted: params.posted,
             slugline: params.slugline,
             state: params.state,
             subject: params.subject,
-            urgency: params.urgency,
             language: params.language,
-            name: params.name,
+        },
+    };
+}
+
+export function planningParamsToSearchParams(params: IPlanningSearchParams): ISearchParams {
+    return {
+        ...commonParamsToSearchParams(params),
+        ad_hoc_planning: params.adHocPlanning,
+        exclude_rescheduled_and_cancelled: params.excludeRescheduledAndCancelled,
+        no_coverage: params.advancedSearch?.noCoverage,
+        urgency: params.advancedSearch?.urgency,
+        g2_content_type: params.advancedSearch?.g2_content_type,
+        featured: params.featured ?? params.advancedSearch?.featured,
+        include_scheduled_updates: params.includeScheduledUpdates,
+        no_agenda_assigned: params.noAgendaAssigned,
+    };
+}
+
+export function searchParamsToPlanningParams(params: ISearchParams): IPlanningSearchParams {
+    const common = searchParamsToCommonParams(params);
+
+    return {
+        ...common,
+        agendas: params.agendas,
+        noAgendaAssigned: params.no_agenda_assigned,
+        adHocPlanning: params.ad_hoc_planning,
+        excludeRescheduledAndCancelled: params.exclude_rescheduled_and_cancelled,
+        featured: params.featured,
+        includeScheduledUpdates: params.include_scheduled_updates,
+        advancedSearch: {
+            ...common.advancedSearch,
+            featured: params.featured,
+            g2_content_type: params.g2_content_type,
+            noCoverage: params.no_coverage,
+            urgency: params.urgency,
         },
     };
 }
 
 export function eventParamsToSearchParams(params: IEventSearchParams): ISearchParams {
     return {
-        item_ids: params.ids,
-        name: params.advancedSearch?.name,
-        full_text: params.fulltext,
-        anpa_category: params.advancedSearch?.anpa_category,
-        subject: params.advancedSearch?.subject,
-        posted: params.advancedSearch?.posted,
-        place: params.advancedSearch?.place,
-        language: params.advancedSearch?.language,
-        state: params.advancedSearch?.state,
-        spike_state: params.spikeState,
-        include_killed: params.includeKilled,
-        date_filter: params.advancedSearch?.dates?.range,
-        start_date: params.advancedSearch?.dates?.start,
-        end_date: params.advancedSearch?.dates?.end,
-        only_future: params.onlyFuture,
-        start_of_week: params.startOfWeek,
-        slugline: params.advancedSearch?.slugline,
+        ...commonParamsToSearchParams(params),
         recurrence_id: params.recurrenceId,
         reference: params.advancedSearch?.reference,
         source: params.advancedSearch?.source,
+        location: params.advancedSearch?.location,
         no_calendar_assigned: params.noCalendarAssigned,
-
-        max_results: params.maxResults,
-        page: params.page,
-
-        // Location in EventSearchParams is ILocation | string
-        // location: params.advancedSearch?.location,
-
-        // Calendars in EventSearchParams is Array<string>
-        // calendars: params.calendars,
+        calendars: params.calendars,
     };
 }
 
 export function searchParamsToEventParams(params: ISearchParams): IEventSearchParams {
+    const common = searchParamsToCommonParams(params);
+
     return {
+        ...common,
+        ids: common.itemIds,
         calendars: params.calendars,
-        fulltext: params.full_text,
-        ids: params.item_ids,
-        includeKilled: params.include_killed,
-        itemIds: params.item_ids,
-        maxResults: params.max_results,
         noCalendarAssigned: params.no_calendar_assigned,
-        onlyFuture: params.only_future,
         recurrenceId: params.recurrence_id,
-        page: params.page,
-        startOfWeek: params.start_of_week,
-        spikeState: params.spike_state,
         advancedSearch: {
-            anpa_category: params.anpa_category,
-            dates: {
-                range: params.date_filter,
-                start: params.start_date,
-                end: params.end_date,
-            },
+            ...common.advancedSearch,
             location: params.location,
-            name: params.name,
-            place: params.place,
-            posted: params.posted,
             reference: params.reference,
-            slugline: params.slugline,
             source: params.source,
-            state: params.state,
-            subject: params.subject,
-            language: params.language,
         },
     };
 }
 
 export function combinedParamsToSearchParams(params: ICombinedSearchParams): ISearchParams {
     return {
-        full_text: params.fulltext,
-        anpa_category: params.advancedSearch?.anpa_category,
-        subject: params.advancedSearch?.subject,
-        posted: params.advancedSearch?.posted,
-        place: params.places ?? params.advancedSearch?.place,
-        state: params.advancedSearch?.state,
-        spike_state: params.spikeState,
-        date_filter: params.advancedSearch?.dates?.range,
-        start_date: params.advancedSearch?.dates?.start,
-        end_date: params.advancedSearch?.dates?.end,
-        slugline: params.advancedSearch?.slugline,
+        ...commonParamsToSearchParams(params),
         reference: params.advancedSearch?.reference,
-        calendars: params.calendars,
-        agendas: (params.agendas ?? []).map((agenda) => agenda._id),
-        include_killed: params.includeKilled,
-
-        max_results: params.maxResults,
-        page: params.page,
     };
 }
 
 export function searchParamsToCombinedParams(params: ISearchParams): ICombinedSearchParams {
+    const common = searchParamsToCommonParams(params);
+
     return {
-        fulltext: params.full_text,
-        spikeState: params.spike_state,
-        page: params.page,
-        maxResults: params.max_results,
-        calendars: params.calendars,
-        agendas: params.agendas,
-        places: params.place,
-        includeKilled: params.include_killed,
+        ...common,
         advancedSearch: {
-            name: params.name,
-            anpa_category: params.anpa_category,
-            subject: params.subject,
-            place: params.place,
-            slugline: params.slugline,
+            ...common.advancedSearch,
             reference: params.reference,
-            state: params.state,
-            posted: params.posted,
-            dates: {
-                range: params.date_filter,
-                start: params.start_date,
-                end: params.end_date,
-            },
         },
     };
 }
