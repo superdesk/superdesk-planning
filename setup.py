cc0aba6ca299504aaba6ac28f45668f419f25f48
Mark Pittaway <mark.pittaway@gmail.com>
2021-02-23 10:28:44 +1100

    Release 2.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ package_data = {
 
 setup(
     name="superdesk-planning",
-    version="2.1.1",
+    version="2.1.2",
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     package_dir={'': 'server'},
