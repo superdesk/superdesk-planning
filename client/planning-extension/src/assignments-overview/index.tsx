b1263907515de1aaebc815ac278016adada490d3
Tomas Kikutis <t.kikutis@gmail.com>
2021-03-25 18:31:15 +0100

    update data after receiving websocket notifications

--- client/planning-extension/src/assignments-overview/index.tsx
+++ client/planning-extension/src/assignments-overview/index.tsx
@@ -1,10 +1,18 @@
 import {groupBy} from 'lodash';
 import * as React from 'react';
-import {IDesk, IRestApiResponse, IVocabulary, IVocabularyItem} from 'superdesk-api';
+import {
+    IDesk,
+    IRestApiResponse,
+    IUser,
+    IVocabulary,
+    IVocabularyItem,
+} from 'superdesk-api';
 import {IAssignmentItem} from '../../../interfaces';
 import {superdesk} from '../superdesk';
 import {Badge} from 'superdesk-ui-framework';
 import {AssignmentsOverviewListItem} from './assignments-overview-list-item';
+
+const {addWebsocketMessageListener} = superdesk;
 
 const DropdownTree = superdesk.components.getDropdownTree<IAssignmentItem>();
 const {queryRawJson, findOne} = superdesk.dataApi;
@@ -12,58 +20,166 @@ const {GroupLabel, IconBig, TopMenuDropdownButton} = superdesk.components;
 
 interface IState {
     loading: false;
+    currentUser: IUser;
     desks: {[key: string]: IDesk}; // desks by _id
     contentTypes: Array<IVocabularyItem>;
     items: Array<IAssignmentItem>;
 }
 
+interface IResourceChange {
+    changeType: 'created' | 'updated' | 'deleted';
+    resource: string;
+    itemId: string;
+    fields?: {[key: string]: 1};
+}
+
+function fetchDesks(): Promise<IState['desks']> {
+    return queryRawJson<IRestApiResponse<IDesk>>('desks').then((desksResponse) => {
+        const desks = desksResponse._items.reduce<{[key: string]: IDesk}>((acc, item) => {
+            acc[item._id] = item;
+
+            return acc;
+        }, {});
+
+        return desks;
+    });
+}
+
+function fetchContentTypes(): Promise<IState['contentTypes']> {
+    return findOne<IVocabulary>('vocabularies', 'g2_content_type').then(({items}) => (items));
+}
+
+function fetchAssignments(userId: IUser['_id']): Promise<IState['items']> {
+    return queryRawJson<IRestApiResponse<IAssignmentItem>>(
+        'assignments',
+        {
+            page: '1',
+            sort: '[("planning.scheduled", 1)]',
+            source: JSON.stringify({
+                query: {
+                    bool: {
+                        must: [
+                            {term: {'assigned_to.user': userId}},
+                            {terms: {'assigned_to.state': ['in_progress', 'submitted']}},
+                        ],
+                    },
+                },
+            }),
+        },
+    ).then(({_items}) => _items);
+}
+
 export class AssignmentsList extends React.PureComponent<{}, {loading: true} | IState> {
+    private eventListenersToRemoveBeforeUnmounting: Array<() => void>;
+
     constructor(props: {}) {
         super(props);
 
         this.state = {loading: true};
+
+        this.eventListenersToRemoveBeforeUnmounting = [];
+
+        this.handleContentChanges = this.handleContentChanges.bind(this);
+
+        this.eventListenersToRemoveBeforeUnmounting.push(
+            addWebsocketMessageListener(
+                'resource:created',
+                (event) => {
+                    const {resource, _id} = event.detail.extra;
+
+                    this.handleContentChanges([{changeType: 'created', resource: resource, itemId: _id}]);
+                },
+            ),
+        );
+
+        this.eventListenersToRemoveBeforeUnmounting.push(
+            addWebsocketMessageListener(
+                'resource:updated',
+                (event) => {
+                    const {resource, _id, fields} = event.detail.extra;
+
+                    this.handleContentChanges([{
+                        changeType: 'updated',
+                        resource: resource,
+                        itemId: _id,
+                        fields: fields,
+                    }]);
+                },
+            ),
+        );
+
+        this.eventListenersToRemoveBeforeUnmounting.push(
+            addWebsocketMessageListener(
+                'resource:deleted',
+                (event) => {
+                    const {resource, _id} = event.detail.extra;
+
+                    this.handleContentChanges([{changeType: 'deleted', resource: resource, itemId: _id}]);
+                },
+            ),
+        );
+    }
+
+    handleContentChanges(changes: Array<IResourceChange>) {
+        const state = this.state;
+
+        if (state.loading === true) {
+            return;
+        }
+
+        const {items} = state;
+
+        const refetchDesks = changes.find(({resource}) => resource === 'desks');
+        const refetchContentTypes = changes.find(
+            ({resource, itemId}) => resource === 'vocabularies' && itemId === 'g2_content_type',
+        );
+        const refetchAssignments = changes.find(
+            ({changeType, resource, itemId}) =>
+                (resource === 'assignments' && (changeType === 'created' || changeType === 'deleted'))
+                || (
+                    resource === 'assignments'
+                    && changeType === 'updated'
+                    && items.find(({_id}) => _id === itemId) != null
+                ),
+        );
+
+        Promise.all([
+            refetchDesks ? fetchDesks() : Promise.resolve(state.desks),
+            refetchContentTypes ? fetchContentTypes() : Promise.resolve(state.contentTypes),
+            refetchAssignments ? fetchAssignments(state.currentUser._id) : Promise.resolve(state.items),
+        ]).then(([desks, contentTypes, items]) => {
+            this.setState({
+                loading: false,
+                desks: desks,
+                contentTypes: contentTypes,
+                items: items,
+            });
+        });
     }
 
     componentDidMount() {
-        const getDesks = () => queryRawJson<IRestApiResponse<IDesk>>('desks');
-        const getContentTypes = () => findOne<IVocabulary>('vocabularies', 'g2_content_type');
-        const getCurrentUser = () => superdesk.session.getCurrentUser();
-
-        Promise.all([
-            getCurrentUser(),
-            getDesks(),
-            getContentTypes(),
-        ])
-            .then(([currentUser, desksResponse, contentTypes]) =>
-                queryRawJson<IRestApiResponse<IAssignmentItem>>(
-                    'assignments',
-                    {
-                        page: '1',
-                        sort: '[("planning.scheduled", 1)]',
-                        source: JSON.stringify({
-                            query: {
-                                bool: {
-                                    must: [
-                                        {term: {'assigned_to.user': currentUser._id}},
-                                        {terms: {'assigned_to.state': ['in_progress', 'submitted']}},
-                                    ],
-                                },
-                            },
-                        }),
-                    },
-                ).then((response) => {
+        superdesk.session.getCurrentUser()
+            .then((currentUser) =>
+                Promise.all([
+                    fetchDesks(),
+                    fetchContentTypes(),
+                    fetchAssignments(currentUser._id),
+                ]).then(([desks, contentTypes, items]) => {
                     this.setState({
                         loading: false,
-                        desks: desksResponse._items.reduce<{[key: string]: IDesk}>((acc, item) => {
-                            acc[item._id] = item;
-
-                            return acc;
-                        }, {}),
-                        contentTypes: contentTypes.items,
-                        items: response._items
+                        currentUser: currentUser,
+                        desks: desks,
+                        contentTypes: contentTypes,
+                        items: items,
                     });
-                }),
+                })
             );
+    }
+
+    componentWillUnmount() {
+        this.eventListenersToRemoveBeforeUnmounting.forEach((removeListener) => {
+            removeListener();
+        });
     }
 
     render() {
