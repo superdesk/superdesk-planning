9fbb0812d5fd84237b1362fa97d593cbe2189f51
Tomas Kikutis <t.kikutis@gmail.com>
2021-03-30 19:01:26 +0200

    remove generated files from dist folder

--- client/planning-extension/dist/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getAssignmentService() {
-    var injector = angular.element(document.body).injector();
-    return injector.get('assignments');
-}
-exports.getAssignmentService = getAssignmentService;
