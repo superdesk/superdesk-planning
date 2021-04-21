4f86a324925c8301c8e9115554a9fce39a909226
Mark Pittaway <mark.pittaway@gmail.com>
2021-04-21 10:13:20 +1000

    Bump cypress/terminal versions Re-enable terminal report

--- e2e/cypress/support/index.ts
+++ e2e/cypress/support/index.ts
@@ -1,3 +1,2 @@
 
-// Disabling for now - see https://github.com/archfz/cypress-terminal-report/issues/63
-// require('cypress-terminal-report/src/installLogsCollector')();
+require('cypress-terminal-report/src/installLogsCollector')();
