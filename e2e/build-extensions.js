b851066702b520cf907c5a421c06706d857780bb
Tomas Kikutis <t.kikutis@gmail.com>
2021-05-25 12:04:47 +0200

    use build tools for e2e tests

--- e2e/build-extensions.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const execSync = require('child_process').execSync;
-const dir = path.join(require.resolve('superdesk-core/package.json'), '../tasks/build-extensions');
-
-execSync(`cp -r ${dir} ./`, {stdio: 'inherit'});
-execSync('node build-extensions/index.js', {stdio: 'inherit'});
-execSync('rm -r build-extensions', {stdio: 'inherit'});
