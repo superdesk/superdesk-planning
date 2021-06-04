de3d13f8cb5257a7d92e9a4e146f88a946e5d48c
MarkLark86 <mark.pittaway@gmail.com>
2021-06-04 14:36:12 +1000

    [SDESK-5982] fix: Force fetch Planning from API after POST (#1591)
    * [SDESK-5982] fix: Force fetch Planning from API after POST
    
    * fix unit tests
    
    * Add e2e tests for SDESK-5982

--- client/interfaces.ts
+++ client/interfaces.ts
@@ -1845,7 +1845,7 @@ export interface IPlanningAPI {
     planning: {
         search(params: ISearchParams): Promise<IRestApiResponse<IPlanningItem>>;
         searchGetAll(params: ISearchParams): Promise<Array<IPlanningItem>>;
-        getById(planId: IPlanningItem['_id'], saveToStore?: boolean): Promise<IPlanningItem>;
+        getById(planId: IPlanningItem['_id'], saveToStore?: boolean, force?: boolean): Promise<IPlanningItem>;
         getByIds(planIds: Array<IPlanningItem['_id']>): Promise<Array<IPlanningItem>>;
         getLocked(): Promise<Array<IPlanningItem>>;
         getLockedFeatured(): Promise<Array<IFeaturedPlanningLock>>;
