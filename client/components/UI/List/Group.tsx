9c4f5ed83eccd6d1830c26c1a987eccd0897c90e
Tomas Kikutis <t.kikutis@gmail.com>
2021-06-16 11:34:21 +0200

    update the component from #1603

--- client/components/UI/List/Group.tsx
+++ client/components/UI/List/Group.tsx
@@ -1,74 +1,72 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import {IAccessibleListBox} from 'components/Main/ListPanel';
 
-/**
- * @ngdoc react
- * @name Group
- * @description Component to group a list of items
- */
-export const Group = (props) => {
-    const {
-        className,
-        children,
-        spaceBetween,
-        verticalScroll,
-        style,
-        onScroll,
-        refNode,
-        shadow,
-        testId,
-        listBoxGroupProps,
-        indexFrom,
-    } = props;
-
-    return (
-        <div
-            aria-labelledby={props['aria-labelledby']}
-            data-test-id={testId}
-            data-index-from={indexFrom}
-            className={classNames(
-                className,
-                'sd-list-item-group',
-                shadow ? `sd-shadow--z${shadow}` : null,
-                {
-                    'sd-list-item-group--space-between-items': spaceBetween,
-                    'sd-list-item-group--vertical-scroll': verticalScroll,
-                }
-            )}
-            style={style}
-            onScroll={onScroll}
-            ref={refNode}
-            {...(listBoxGroupProps?.containerProps ?? {})}
-        >
-            {children}
-        </div>
-    );
-};
-
-Group.propTypes = {
-    className: PropTypes.string,
-    children: PropTypes.node.isRequired,
-    spaceBetween: PropTypes.bool,
-    verticalScroll: PropTypes.bool,
-    style: PropTypes.object,
-    onScroll: PropTypes.func,
-    refNode: PropTypes.func,
-    shadow: PropTypes.oneOf([1, 2, 3, 4]),
-    testId: PropTypes.string,
-    listBoxGroupProps: PropTypes.object,
-    'aria-labelledby': PropTypes.string,
+interface IProps {
+    className?: string;
+    spaceBetween?: boolean;
+    verticalScroll?: boolean;
+    style?: React.CSSProperties;
+    refNode?: any;
+    shadow?: number;
+    testId?: string;
+    'aria-labelledby'?: string;
+    tabIndex?: number;
 
     /**
      * Active item index is shared between groups.
      * When focus goes to another group, active index needs to be set accordingly.
      * indexFrom indicates global index of the first item in a given group.
      */
-    indexFrom: PropTypes.number,
-};
+    indexFrom?: number;
 
-Group.defaultProps = {
-    spaceBetween: false,
-    verticalScroll: false,
-    style: {},
-};
+    listBoxGroupProps: IAccessibleListBox;
+
+    onScroll?(event: React.UIEvent): void;
+    onKeyDown?(event: React.KeyboardEvent<HTMLUListElement>): void;
+}
+
+export class Group extends React.PureComponent<IProps> {
+    render() {
+        const {
+            className,
+            children,
+            spaceBetween,
+            verticalScroll,
+            style,
+            onScroll,
+            refNode,
+            shadow,
+            testId,
+            onKeyDown,
+            tabIndex,
+            indexFrom,
+            listBoxGroupProps,
+        } = this.props;
+
+        return (
+            <ul
+                aria-labelledby={this.props['aria-labelledby']}
+                data-test-id={testId}
+                data-index-from={indexFrom}
+                className={classNames(
+                    className,
+                    'sd-list-item-group',
+                    shadow ? `sd-shadow--z${shadow}` : null,
+                    {
+                        'sd-list-item-group--space-between-items': spaceBetween,
+                        'sd-list-item-group--vertical-scroll': verticalScroll,
+                    }
+                )}
+                style={style}
+                onScroll={onScroll}
+                ref={refNode}
+                onKeyDown={onKeyDown}
+                {...(listBoxGroupProps?.containerProps ?? {})}
+                tabIndex={listBoxGroupProps?.containerProps?.tabIndex ?? tabIndex}
+            >
+                {children}
+            </ul>
+        );
+    }
+}
