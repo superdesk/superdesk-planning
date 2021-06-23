29c1022df8bca4e24231dc9353b01f3e6db9ea6b
Tanuj Soni <tanuj.soni@sourcefabric.org>
2021-06-23 13:54:13 +0530

    SDBELGA-568 ADD: Don't allow user to create coverages from an event if they do not have privileges.

--- client/api/editor/item_events.ts
+++ client/api/editor/item_events.ts
@@ -24,7 +24,7 @@ export function getEventsInstance(type: EDITOR_TYPE): IEditorAPI['item']['events
         const {gettext} = superdeskApi.localization;
         const profile = planningApi.events.getEditorProfile();
 
-        return {
+        const groups: {[key: string]: IEditorFormGroup} = {
             schedule: {
                 id: 'schedule',
                 index: 0,
@@ -83,14 +83,19 @@ export function getEventsInstance(type: EDITOR_TYPE): IEditorAPI['item']['events
                     'links',
                 ]),
             },
-            'add-planning': {
+        };
+
+        if (superdeskApi.privileges.hasPrivilege('planning_planning_management')) {
+            groups['add-planning'] = {
                 id: 'add-planning',
                 index: 6,
                 fields: filterProfileForEnabledFields(profile, [
                     'related_plannings',
                 ]),
-            },
-        };
+            };
+        }
+
+        return groups;
     }
 
     function getGroupsForItem(item: Partial<IEventItem>): {
@@ -99,6 +104,7 @@ export function getEventsInstance(type: EDITOR_TYPE): IEditorAPI['item']['events
     } {
         const {gettext} = superdeskApi.localization;
         const groups = getGroups();
+        const canCreatePlanningItems = superdeskApi.privileges.hasPrivilege('planning_planning_management');
 
         return {
             bookmarks: [{
@@ -163,11 +169,13 @@ export function getEventsInstance(type: EDITOR_TYPE): IEditorAPI['item']['events
                 id: 'add_planning',
                 type: BOOKMARK_TYPE.custom,
                 index: 7,
+                disabled: !canCreatePlanningItems,
                 component: AddPlanningBookmark,
             }, {
                 id: 'associated_plannings',
                 type: BOOKMARK_TYPE.custom,
                 index: 8,
+                disabled: !canCreatePlanningItems,
                 component: AssociatedPlanningsBookmark,
             }],
             groups: Object.values(groups),
