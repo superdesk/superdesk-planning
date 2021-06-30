6370fdb1a900ef294835ef85cdfd214c8607886e
Tanuj Soni <tanuj.soni@sourcefabric.org>
2021-06-30 13:47:46 +0530

    SDESK 5906 UPDATE: Events already existing in the database should not be updated

--- server/planning/feed_parsers/superdesk_event_json.py
+++ server/planning/feed_parsers/superdesk_event_json.py
@@ -2,6 +2,7 @@ import logging
 
 from superdesk.io.feed_parsers import FileFeedParser
 from superdesk import get_resource_service
+from superdesk.errors import ParserError
 from superdesk.io.subjectcodes import get_subjectcodeitems
 from superdesk.utc import utcnow
 from planning.common import WORKFLOW_STATE
@@ -38,6 +39,12 @@ class EventJsonFeedParser(FileFeedParser):
         self.items = []
         with open(file_path, 'r') as f:
             superdesk_event = json.load(f)
+
+            events_service = get_resource_service('events')
+            existing_event = events_service.find_one(req=None, guid=superdesk_event.get('guid'))
+            if existing_event:
+                raise ParserError.parseMessageError('An event already exists with exact same Id. Updating events is not supported yet.')
+
         self.items.append(self._transform_from_superdesk_event(superdesk_event))
         return self.items
 
