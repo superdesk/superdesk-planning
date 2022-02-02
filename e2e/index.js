f70c7b7cf99b64e155ffddfededaa9032d199bd3
Mark Pittaway <mark.pittaway@gmail.com>
2022-02-02 17:04:36 +1100

    fix(e2e): Use older method of loading extensions

--- e2e/index.js
+++ e2e/index.js
@@ -1,14 +1,9 @@
 import {startApp} from 'superdesk-core/scripts/index';
+import planningExtension from 'superdesk-planning/client/planning-extension/dist/extension';
 
 setTimeout(() => {
     startApp(
-        [{
-            id: 'planning',
-            load: () => (
-                import('superdesk-planning/client/planning-extension/dist/extension')
-                    .then((res) => res.default)
-            ),
-        }],
+        [planningExtension],
         {}
     );
 });
