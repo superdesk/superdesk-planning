5d58e8559c273c51880313112b6b07feba5f18ae
GyanP <gyanig72@gmail.com>
2022-06-17 11:54:16 +0530

    Planning: As a SD admin I want to ingest planning items [SDESK-6480] (#1690)
    * Planning: As a SD admin I want to ingest planning items [SDESK-6480]
    
    * Parse and map fields for planning
    
    * fix black and add test cases
    
    * minor change
    
    * minor change
    
    * Address the comments
    
    * minor change

--- server/planning/feed_parsers/__init__.py
+++ server/planning/feed_parsers/__init__.py
@@ -12,7 +12,9 @@ from superdesk.io.registry import register_feed_parser
 from .ics_2_0 import IcsTwoFeedParser
 from .superdesk_event_json import EventJsonFeedParser
 from .events_ml import EventsMLParser
+from .superdesk_planning_xml import PlanningMLParser
 
 register_feed_parser(IcsTwoFeedParser.NAME, IcsTwoFeedParser())
 register_feed_parser(EventJsonFeedParser.NAME, EventJsonFeedParser())
 register_feed_parser(EventsMLParser.NAME, EventsMLParser())
+register_feed_parser(PlanningMLParser.NAME, PlanningMLParser())
