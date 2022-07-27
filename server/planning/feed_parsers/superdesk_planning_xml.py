f6f63776f2f273a18b64d1055fd92ad57497aa47
Mark Pittaway <mark.pittaway@gmail.com>
2022-07-27 16:43:02 +1000

    fix(ingest): Missing coverage schedules and other metadata

--- server/planning/feed_parsers/superdesk_planning_xml.py
+++ server/planning/feed_parsers/superdesk_planning_xml.py
@@ -1,11 +1,10 @@
-from cgitb import text
-from datetime import date
 import logging
 from eve.utils import config
+from flask import current_app as app
+
 from superdesk.io.feed_parsers import NewsMLTwoFeedParser
 import pytz
 from xml.etree.ElementTree import Element
-from flask import current_app as app
 from superdesk import get_resource_service
 from superdesk.metadata.item import (
     CONTENT_TYPE,
@@ -15,6 +14,8 @@ from superdesk.metadata.item import (
 )
 from superdesk.utc import utcnow, utc_to_local
 from superdesk.errors import ParserError
+
+from planning.common import get_coverage_status_from_cv, get_default_coverage_status_qcode_on_ingest, WORKFLOW_STATE
 
 utc = pytz.UTC
 logger = logging.getLogger(__name__)
@@ -54,8 +55,19 @@ class PlanningMLParser(NewsMLTwoFeedParser):
         except Exception:
             return False
 
+    def set_missing_voc_policy(self):
+        # config is not accessible during __init__, so we check it here
+        if self.__class__.missing_voc is None:
+            self.__class__.missing_voc = app.config.get("QCODE_MISSING_VOC", "continue")
+            if self.__class__.missing_voc not in ("reject", "create", "continue"):
+                logger.warning(
+                    'Bad QCODE_MISSING_VOC value ({value}) using default ("continue")'.format(value=self.missing_voc)
+                )
+                self.__class__.missing_voc = "continue"
+
     def parse(self, tree: Element, provider=None):
         self.root = tree
+        self.set_missing_voc_policy()
 
         try:
             guid = tree.attrib["guid"]
@@ -75,8 +87,9 @@ class PlanningMLParser(NewsMLTwoFeedParser):
 
             self.parse_item_meta(tree, item)
             self.parse_content_meta(tree, item)
-            self.parse_news_coverage_status(tree, item)
             self.parse_news_coverage_set(tree, item)
+            self.parse_news_coverage_status(tree, item)
+
             return [item]
 
         except Exception as ex:
@@ -91,12 +104,14 @@ class PlanningMLParser(NewsMLTwoFeedParser):
         meta = tree.find(self.qname("itemMeta"))
 
         versioncreated_elt = meta.find(self.qname("versionCreated"))
-        editor_note = meta.find(self.qname("edNote"))
-        item_class_elt = meta.find(self.qname("itemClass"))
         if versioncreated_elt is not None and versioncreated_elt.text:
             item["versioncreated"] = self.datetime(meta.find(self.qname("versionCreated")).text)
+
+        editor_note = meta.find(self.qname("edNote"))
         if editor_note is not None and editor_note.text:
             item["ednote"] = editor_note.text
+
+        item_class_elt = meta.find(self.qname("itemClass"))
         if item_class_elt is not None:
             item["item_class"] = item_class_elt.get("qcode")
 
@@ -114,9 +129,10 @@ class PlanningMLParser(NewsMLTwoFeedParser):
         if firstcreated_elt is not None and firstcreated_elt.text:
             item["firstcreated"] = self.datetime(firstcreated_elt.text)
 
-        updated_elt = content_meta.find(self.qname("contentModified"))
-        if updated_elt is not None and updated_elt.text:
-            item["_updated"] = self.datetime(updated_elt.text)
+        if not item["versioncreated"]:
+            updated_elt = content_meta.find(self.qname("contentModified"))
+            if updated_elt is not None and updated_elt.text:
+                item["versioncreated"] = self.datetime(updated_elt.text)
 
         slugline_elt = content_meta.find(self.qname("headline"))
         if slugline_elt is not None and slugline_elt.text:
@@ -137,19 +153,25 @@ class PlanningMLParser(NewsMLTwoFeedParser):
             item["planning_date"] = utc_to_local(config.DEFAULT_TIMEZONE, utcnow())
 
     def parse_news_coverage_status(self, tree, item):
-        """Parse assert tag
+        """Parse newsCoverageStatus tag
 
         :param tree: tree
         :param item: planning item
         """
+
+        # Get the ``news_coverage_status.qcode`` to use
         assert_el = tree.find(self.qname("assert"))
+        status_qcode = get_default_coverage_status_qcode_on_ingest()
         if assert_el is not None:
             news_coverage_status = assert_el.find(self.qname("newsCoverageStatus")).get("qcode")
-            item["news_coverage_status"] = {
-                "label": "planned",
-                "name": "coverage intended",
-                "qcode": news_coverage_status,
-            }
+            if news_coverage_status:
+                status_qcode = news_coverage_status
+
+        # Now assign the ``news_coverage_status`` to all coverages of this Planning item
+        coverage_status = get_coverage_status_from_cv(status_qcode)
+        coverage_status.pop("is_active", None)
+        for coverage in item["coverages"]:
+            coverage["news_coverage_status"] = coverage_status
 
     def parse_genre(self, planning_elt, planning):
         """Parse Genre tag
@@ -162,7 +184,7 @@ class PlanningMLParser(NewsMLTwoFeedParser):
             genre_name = genre_elt.find(self.qname("name"))
             planning["genre"] = [{"qcode": genre_elt.get("qcode"), "name": genre_name.text}]
 
-    def parse_coverage_planning(self, news_coverage_elt):
+    def parse_coverage_planning(self, news_coverage_elt, item):
         """Map news coverage with planning
 
         :param news_coverage_elt: news coverage element
@@ -171,16 +193,23 @@ class PlanningMLParser(NewsMLTwoFeedParser):
         if planning_elt is not None:
             headline_elt = planning_elt.find(self.qname("headline"))
             content = planning_elt.find(self.qname("itemClass")).get("qcode")
-            planning = {"slugline": headline_elt.text, "g2_content_type": content.split(":")[1]}
+            planning = {"slugline": headline_elt.text.strip(), "g2_content_type": content.split(":")[1]}
+
+            description_elt = planning_elt.find(self.qname("description"))
+            if description_elt is not None and description_elt.text:
+                planning["description_text"] = description_elt.text
 
             scheduled_elt = planning_elt.find(self.qname("scheduled"))
             if scheduled_elt is not None and scheduled_elt.text:
                 planning["scheduled"] = self.datetime(scheduled_elt.text)
+            else:
+                # If no scheduling details are found, fallback to Planning items date
+                planning["scheduled"] = item["planning_date"]
 
             self.parse_genre(planning_elt, planning)
             return planning
 
-        return {}
+        return None
 
     def parse_news_coverage_set(self, tree, item):
         """Parse newsCoverageSet tag
@@ -190,12 +219,22 @@ class PlanningMLParser(NewsMLTwoFeedParser):
         """
         item["coverages"] = []
         news_coverage_set = tree.find(self.qname("newsCoverageSet"))
-        if news_coverage_set:
+        if news_coverage_set is not None:
             for news_coverage_elt in news_coverage_set.findall(self.qname("newsCoverage")):
+                coverage_id = news_coverage_elt.get("id")
+                planning = self.parse_coverage_planning(news_coverage_elt, item)
+
+                if planning is None:
+                    logger.warning(f"Failed to process coverage '{coverage_id}', planning details not found")
+                    continue
+
+                modified = news_coverage_elt.get("modified")
                 item["coverages"].append(
                     {
-                        "coverage_id": news_coverage_elt.get("id"),
-                        "firstcreated": self.datetime(news_coverage_elt.get("modified")),
-                        "planning": self.parse_coverage_planning(news_coverage_elt),
+                        "coverage_id": coverage_id,
+                        "workflow_status": WORKFLOW_STATE.DRAFT,
+                        "firstcreated": item["firstcreated"],
+                        "versioncreated": self.datetime(modified) if modified else item["firstcreated"],
+                        "planning": planning,
                     }
                 )
