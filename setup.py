0548393b244224872742144f06bc6d14d71f61e6
Mark Pittaway <mark.pittaway@gmail.com>
2022-09-05 17:56:10 +1000

    Set version to 2.6.0-dev

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ package_data = {
 
 setup(
     name="superdesk-planning",
-    version="2.5.0-dev",
+    version="2.6.0-dev",
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     package_dir={'': 'server'},
