50783c6c6f7f73344675b82a8d1d969feea900b3
Tomas Kikutis <t.kikutis@gmail.com>
2023-05-08 11:46:00 +0200

    review and correct displayed data to match existing code only design should change, not data itself

--- client/components/Coverages/CoverageIcons.tsx
+++ client/components/Coverages/CoverageIcons.tsx
@@ -5,10 +5,12 @@ import * as config from 'appConfig';
 import {IPlanningCoverageItem, IG2ContentType, IContactItem, IPlanningConfig} from '../../interfaces';
 import {IUser, IDesk} from 'superdesk-api';
 import {gettext} from 'superdesk-core/scripts/core/utils';
-import {AvatarGroup, ContentDivider, Icon, WithPopover, Avatar, AvatarPlaceholder, Spacer, SpacerBlock} from 'superdesk-ui-framework/react';
+import {AvatarGroup, ContentDivider, Icon, WithPopover, Avatar, AvatarPlaceholder, Spacer} from 'superdesk-ui-framework/react';
 import {IPropsAvatarPlaceholder} from 'superdesk-ui-framework/react/components/avatar/avatar-placeholder';
 import {IPropsAvatar} from 'superdesk-ui-framework/react/components/avatar/avatar';
 import {trimStartExact} from 'superdesk-core/scripts/core/helpers/utils';
+import {getVocabularyItemFieldTranslated} from '../../utils/vocabularies';
+import {getUserInterfaceLanguageFromCV} from '../../utils/users';
 import {planningUtils} from '../../utils';
 import './coverage-icons.scss';
 
@@ -23,8 +25,6 @@ interface IProps {
 }
 
 const appConfig = config.appConfig as IPlanningConfig;
-const dateFormat = appConfig.planning.dateformat;
-const timeFormat = appConfig.planning.timeformat;
 
 export function isAvatarPlaceholder(
     item: Omit<IPropsAvatar, 'size'> | Omit<IPropsAvatarPlaceholder, 'size'>
@@ -73,42 +73,56 @@ export class CoverageIcons extends React.PureComponent<IProps> {
                     <div className="coverages-popup">
                         <Spacer v gap="16">
                             {this.props.coverages.map((coverage, i) => {
-                                let scheduledStr = coverage.planning?.scheduled != null && dateFormat && timeFormat
-                                    ? moment(coverage.planning.scheduled).format(dateFormat + ' ' + timeFormat)
-                                    : null;
+                                const language = coverage.planning?.language ?? getUserInterfaceLanguageFromCV();
+                                const desk = this.props.desks.find(
+                                    (d) => d._id === coverage.assigned_to?.desk,
+                                );
+                                const dateFormat = appConfig.planning.dateformat;
+                                const timeFormat = appConfig.planning.timeformat;
+
+                                const assignmentStr = desk ?
+                                    gettext('Desk: {{ desk }}', {desk: desk.name}) :
+                                    gettext('Status: Unassigned');
+                                let scheduledStr = coverage.planning?.scheduled != null && dateFormat && timeFormat ?
+                                    moment(coverage.planning.scheduled).format(dateFormat + ' ' + timeFormat) :
+                                    null;
 
                                 if (coverage._time_to_be_confirmed) {
                                     scheduledStr = moment(coverage.planning.scheduled)
                                         .format(dateFormat + ` @ ${gettext('TBC')}`);
                                 }
-
-                                const user = users.find(
-                                    (u) => u._id === coverage.assigned_to?.user,
-                                );
-
-                                const desk = this.props.desks.find(
-                                    (d) => d._id === coverage.assigned_to?.desk,
-                                );
-
                                 const slugline = coverage.planning?.slugline ?? '';
+                                const contentType = getVocabularyItemFieldTranslated(
+                                    this.props.contentTypes.find(
+                                        (type) => type.qcode === coverage.planning?.g2_content_type
+                                    ),
+                                    'name',
+                                    language
+                                );
 
                                 const maybeAvatar = getAvatarForCoverage(coverage, users);
 
                                 return (
                                     <Spacer h gap="8" noWrap key={i}>
                                         <div>
-                                            <Icon
-                                                size="small"
-                                                name={trimStartExact(planningUtils.getCoverageIcon(
-                                                    planningUtils.getCoverageContentType(
-                                                        coverage, this.props.contentTypes
-                                                    )
-                                                    || coverage.planning?.g2_content_type, coverage
-                                                ), 'icon-')}
-                                                color={trimStartExact(
-                                                    planningUtils.getCoverageIconColor(coverage), 'icon--')
-                                                }
-                                            />
+                                            <span title={gettext('Type: {{ type }}', {type: contentType})}>
+                                                <Icon
+                                                    size="small"
+                                                    name={trimStartExact(
+                                                        planningUtils.getCoverageIcon(
+                                                            planningUtils.getCoverageContentType(
+                                                                coverage, this.props.contentTypes
+                                                            ) || coverage.planning?.g2_content_type,
+                                                            coverage,
+                                                        ),
+                                                        'icon-',
+                                                    )}
+                                                    color={trimStartExact(
+                                                        planningUtils.getCoverageIconColor(coverage),
+                                                        'icon--',
+                                                    )}
+                                                />
+                                            </span>
                                         </div>
 
                                         <div>
@@ -121,17 +135,29 @@ export class CoverageIcons extends React.PureComponent<IProps> {
                                                 </span>
                                             </div>
 
-                                            <Spacer h gap="4" noWrap>
-                                                {!desk ? null : (
-                                                    <div>
-                                                        <span className="coverages-popup__text-light">
-                                                            {gettext('Due:')}
-                                                            <span className="coverages-popup__text-bold">
-                                                                {desk.desk_type}
+                                            {(coverage.scheduled_updates ?? []).map((s) => {
+                                                if (s.planning?.scheduled != null) {
+                                                    const scheduledStr2 = dateFormat && timeFormat ?
+                                                        moment(s.planning.scheduled).format(dateFormat + ' ' + timeFormat) :
+                                                        null;
+
+                                                    return (
+                                                        <div>
+                                                            <span className="coverages-popup__text-light">
+                                                                {gettext('Update Due:')}
+                                                                <span className="coverages-popup__text-bold">
+                                                                    {scheduledStr2}
+                                                                </span>
                                                             </span>
-                                                        </span>
-                                                    </div>
-                                                )}
+                                                        </div>
+                                                    );
+                                                }
+
+                                                return null;
+                                            })}
+
+                                            <Spacer h gap="4" noWrap>
+                                                <div>{assignmentStr}</div>
 
                                                 <div style={{flexShrink: 1}}>
                                                     <ContentDivider margin="x-small" orientation="vertical" type="dashed" />
