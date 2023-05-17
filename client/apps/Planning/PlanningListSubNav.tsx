2258a9ba5fb174d0dfb2f6578061b27871676322
devketanpro <devketanpro11@gmail.com>
2023-05-17 17:28:25 +0530

    update class name

--- client/apps/Planning/PlanningListSubNav.tsx
+++ client/apps/Planning/PlanningListSubNav.tsx
@@ -203,7 +203,7 @@ class PlanningListSubNavComponent extends React.Component<IProps, IState> {
                         </div>
                     )}
 
-                    <ButtonGroup className = "btnGroup" align="end">
+                    <ButtonGroup className = "hideOnMobile" align="end">
                         {this.props.listViewType === LIST_VIEW_TYPE.LIST ? (
                             <React.Fragment>
                                 <div
