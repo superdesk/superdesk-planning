3164dc8397850beeb27da405507d330834159181
dzonidoo <nikola71092@gmail.com>
2024-01-23 14:41:30 +0100

    events planning filter

--- client/components/Main/FiltersBox.tsx
+++ client/components/Main/FiltersBox.tsx
@@ -7,20 +7,28 @@ import {StretchBar} from '../UI/SubNav';
 
 import {PLANNING_VIEW} from '../../interfaces';
 import {activeFilter as getCurrentView} from '../../selectors/main';
-import {planningApi} from '../../superdeskApi';
+import {planningApi, superdeskApi} from '../../superdeskApi';
 import {PRIVILEGES} from '../../constants';
+import * as selectors from '../../selectors';
 
 interface IProps {
     showFilters?: boolean; // defaults to true
     currentView: PLANNING_VIEW;
     privileges: {[key: string]: number};
+    currentFilterId?: any;
 }
 
 const mapStateToProps = (state) => ({
     currentView: getCurrentView(state),
+    currentFilterId: selectors.main.currentSearchFilterId(state),
 });
 
 class FiltersBoxComponent extends React.PureComponent<IProps> {
+    componentDidUpdate(prevProps: Readonly<IProps>, prevState: Readonly<{}>, snapshot?: any): void {
+        const {urlParams} = superdeskApi.browser.location;
+        urlParams.setString('eventsPlanningFilter', this.props.currentFilterId);
+    }
+
     render() {
         const privileges = this.props.privileges;
         let filter_items = [];
