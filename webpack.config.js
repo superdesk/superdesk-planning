e6d4c7ed0f70c7ebd020a5a8db5cb2eebb0a41cc
Mark Pittaway <mark.pittaway@gmail.com>
2024-01-24 15:16:25 +1100

    fix front-end tests

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(ts|tsx)?$/,
-                exclude: /node_modules\/(?!(superdesk-core)\/).*/,
+                exclude: /node_modules\/(?!(superdesk-core|domhandler)\/).*/,
                 loader: 'ts-loader',
                 options: {
                     transpileOnly: true
@@ -34,7 +34,7 @@ module.exports = {
             },
             {
                 test: /\.(js|jsx)?$/,
-                exclude: /node_modules\/(?!(superdesk-core)\/).*/,
+                exclude: /node_modules\/(?!(superdesk-core|domhandler)\/).*/,
                 loader: 'ts-loader',
                 options: {
                     transpileOnly: true
