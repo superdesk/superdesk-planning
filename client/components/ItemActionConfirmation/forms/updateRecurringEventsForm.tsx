5beb238c811394a0651f857a293fb285fd145a27
Ketan <73937490+devketanpro@users.noreply.github.com>
2024-02-28 18:22:33 +0530

    Fix: Empty modal showing up when we add a coverage to an event [SDBELGA-769] (#1921)
    * fix typo
    
    * fix lint

--- client/components/ItemActionConfirmation/forms/updateRecurringEventsForm.tsx
+++ client/components/ItemActionConfirmation/forms/updateRecurringEventsForm.tsx
@@ -91,7 +91,7 @@ function getRecurringPlanningToUpdate(
             if (planningItem._id.startsWith(TEMP_ID_PREFIX)) {
                 // This is a temporary Planning, therefor is not part of a recurring series of items
                 return false;
-            } else if (planningItem.planning_recurrence_id == null) {
+            } else if (planningItem.recurrence_id == null) {
                 // This Planning item part of a recurring series of items
                 return false;
             }
@@ -99,7 +99,6 @@ function getRecurringPlanningToUpdate(
             const embeddedCoverages = (planningItem.coverages ?? []).reduce(
                 (embeddedCoverages, coverage) => {
                     embeddedCoverages[coverage.coverage_id] = eventUtils.convertCoverageToEventEmbedded(coverage);
-
                     return embeddedCoverages;
                 },
                 {}
