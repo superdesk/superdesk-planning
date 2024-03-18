b1e05f727694a946c8d3e9607e9220bc18ff9050
Petr Jašek <jasekpetr@gmail.com>
2024-03-18 15:18:55 +0100

    add related items field to event profile config (#1932)
    SDBELGA-767

--- server/planning/content_profiles/profiles/event.py
+++ server/planning/content_profiles/profiles/event.py
@@ -52,6 +52,7 @@ class EventSchema(BaseSchema):
     accreditation_info = TextField(field_type="single_line")
     accreditation_deadline = DateTimeField()
     priority = schema.IntegerField()
+    related_items = schema.ListField()
 
 
 DEFAULT_EVENT_PROFILE = {
@@ -177,6 +178,7 @@ DEFAULT_EVENT_PROFILE = {
         "invitation_details": {"enabled": False},
         "accreditation_info": {"enabled": False},
         "accreditation_deadline": {"enabled": False},
+        "related_items": {"enabled": False},
     },
     "schema": dict(EventSchema),  # type: ignore
     "groups": {
