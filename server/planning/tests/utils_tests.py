9f1f96049b552b220c811d787de7d466af974fc2
devketanpro <devketanpro11@gmail.com>
2024-05-22 11:40:10 +0530

    Refactor date formatting for timezone conversion

--- server/planning/tests/utils_tests.py
+++ server/planning/tests/utils_tests.py
@@ -2,36 +2,37 @@ from planning.tests import TestCase
 from datetime import datetime
 from planning.utils import get_event_formatted_dates
 from planning.search.queries import elastic
+import pytz
 
 
 class TestGetEventFormattedDates(TestCase):
     def test_multi_day_event(self):
-        start = datetime(2024, 5, 28, 5, 00, 00)
-        end = datetime(2024, 5, 29, 6, 00, 00)
-        event = {"dates": {"start": start, "end": end}}
+        start = datetime(2024, 5, 28, 5, 00, 00, tzinfo=pytz.utc)
+        end = datetime(2024, 5, 29, 6, 00, 00, tzinfo=pytz.utc)
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "05:00 28/05/2024 - 06:00 29/05/2024")
+        self.assertEqual(result, "10:30 28/05/2024 - 11:30 29/05/2024")
 
     def test_all_day_event(self):
-        start = datetime(2024, 4, 27, 22, 00, 00)
-        end = datetime(2024, 4, 28, 21, 59, 59)
-        event = {"dates": {"start": start, "end": end}}
+        start = datetime(2024, 4, 27, 22, 00, 00, tzinfo=pytz.utc)
+        end = datetime(2024, 4, 28, 21, 59, 59, tzinfo=pytz.utc)
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "ALL DAY 27/04/2024")
+        self.assertEqual(result, "ALL DAY 28/04/2024")
 
     def test_same_start_end(self):
-        start = datetime(2024, 4, 1, 14, 45)
-        end = datetime(2024, 4, 1, 14, 45)
-        event = {"dates": {"start": start, "end": end}}
+        start = datetime(2024, 4, 1, 14, 45, tzinfo=pytz.utc)
+        end = datetime(2024, 4, 1, 14, 45, tzinfo=pytz.utc)
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "14:45 01/04/2024")
+        self.assertEqual(result, "20:15 01/04/2024")
 
     def test_dates_same_and_different_time(self):
-        start = datetime(2024, 5, 28, 6, 00, 00)
-        end = datetime(2024, 5, 28, 5, 00, 00)
-        event = {"dates": {"start": start, "end": end}}
+        start = datetime(2024, 5, 28, 5, 00, 00, tzinfo=pytz.utc)
+        end = datetime(2024, 5, 28, 6, 00, 00, tzinfo=pytz.utc)
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "06:00 - 05:00, 28/05/2024")
+        self.assertEqual(result, "10:30 - 11:30, 28/05/2024")
 
 
 class TestDateRangeFunctions(TestCase):
--- server/planning/utils.py
+++ server/planning/utils.py
@@ -2,10 +2,11 @@ from typing import Union, List, Dict, Any, TypedDict
 from bson.objectid import ObjectId
 from bson.errors import InvalidId
 from datetime import datetime
-from flask_babel import format_time, format_datetime, lazy_gettext
+from flask_babel import lazy_gettext
 from eve.utils import str_to_date
 import arrow
 from flask import current_app as app
+import pytz
 
 
 class FormattedContact(TypedDict):
@@ -66,32 +67,36 @@ def parse_date(datetime: Union[str, datetime]) -> datetime:
     return datetime
 
 
-def time_short(datetime: datetime):
+def time_short(datetime: datetime, tz: pytz.BaseTzInfo):
     if datetime:
-        return format_time(parse_date(datetime), app.config.get("TIME_FORMAT_SHORT", "HH:mm"))
+        return parse_date(datetime).astimezone(tz).strftime(app.config.get("TIME_FORMAT_SHORT", "%H:%M"))
 
 
-def date_short(datetime: datetime):
+def date_short(datetime: datetime, tz: pytz.BaseTzInfo):
     if datetime:
-        return format_datetime(parse_date(datetime), app.config.get("DATE_FORMAT_SHORT", "dd/MM/yyyy"))
+        return parse_date(datetime).astimezone(tz).strftime(app.config.get("DATE_FORMAT_SHORT", "%d/%m/%Y"))
 
 
 def get_event_formatted_dates(event: Dict[str, Any]) -> str:
     start = event.get("dates", {}).get("start")
     end = event.get("dates", {}).get("end")
+    tz_name = event.get("dates", {}).get("tz") or app.config.get("DEFAULT_TIMEZONE")
+    tz = pytz.timezone(tz_name)
 
     duration_seconds = int((end - start).total_seconds())
 
     if duration_seconds == ALL_DAY_SECONDS:
         # All day event
-        return "{} {}".format(lazy_gettext("ALL DAY"), date_short(start))
+        return "{} {}".format(lazy_gettext("ALL DAY"), date_short(start, tz))
 
     if duration_seconds >= MULTI_DAY_SECONDS:
         # Multi day event
-        return "{} {} - {} {}".format(time_short(start), date_short(start), time_short(end), date_short(end))
+        return "{} {} - {} {}".format(
+            time_short(start, tz), date_short(start, tz), time_short(end, tz), date_short(end, tz)
+        )
 
     if start == end:
         # start and end are the same
-        return "{} {}".format(time_short(start), date_short(start))
+        return "{} {}".format(time_short(start, tz), date_short(start, tz))
 
-    return "{} - {}, {}".format(time_short(start), time_short(end), date_short(start))
+    return "{} - {}, {}".format(time_short(start, tz), time_short(end, tz), date_short(start, tz))
