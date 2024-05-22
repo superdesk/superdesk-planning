351f820f9a02cba5187f707b570b192ab9f116eb
devketanpro <devketanpro11@gmail.com>
2024-05-22 15:49:20 +0530

    update tests output

--- server/planning/tests/utils_tests.py
+++ server/planning/tests/utils_tests.py
@@ -10,28 +10,28 @@ class TestGetEventFormattedDates(TestCase):
         end = datetime(2024, 5, 29, 6, 00, 00)
         event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "05:00 28/05/2024 - 06:00 29/05/2024")
+        self.assertEqual(result, "10:30 28/05/2024 - 11:30 29/05/2024")
 
     def test_all_day_event(self):
         start = datetime(2024, 4, 27, 18, 30, 00)
         end = datetime(2024, 4, 28, 18, 29, 59)
         event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "ALL DAY 27/04/2024")
+        self.assertEqual(result, "ALL DAY 28/04/2024")
 
     def test_same_start_end(self):
         start = datetime(2024, 4, 1, 14, 45)
         end = datetime(2024, 4, 1, 14, 45)
         event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "14:45 01/04/2024")
+        self.assertEqual(result, "20:15 01/04/2024")
 
     def test_dates_same_and_different_time(self):
         start = datetime(2024, 5, 28, 5, 00, 00)
         end = datetime(2024, 5, 28, 6, 00, 00)
         event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "05:00 - 06:00, 28/05/2024")
+        self.assertEqual(result, "10:30 - 11:30, 28/05/2024")
 
 
 class TestDateRangeFunctions(TestCase):
