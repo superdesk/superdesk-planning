65b8c359e3d0d93aab28d179419ccaa9aa355d54
Ketan <73937490+devketanpro@users.noreply.github.com>
2024-05-22 17:47:16 +0530

    Fix : Wrong date in assignment email [SDBELGA-801] (#1990)
    * Refactor date formatting for timezone conversion
    
    * refactore code
    
    * fix mypy
    
    * udpate types
    
    * refactore code
    
    * remove tzinfo from tests
    
    * update tests output

--- server/planning/tests/utils_tests.py
+++ server/planning/tests/utils_tests.py
@@ -8,30 +8,30 @@ class TestGetEventFormattedDates(TestCase):
     def test_multi_day_event(self):
         start = datetime(2024, 5, 28, 5, 00, 00)
         end = datetime(2024, 5, 29, 6, 00, 00)
-        event = {"dates": {"start": start, "end": end}}
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "05:00 28/05/2024 - 06:00 29/05/2024")
+        self.assertEqual(result, "10:30 28/05/2024 - 11:30 29/05/2024")
 
     def test_all_day_event(self):
-        start = datetime(2024, 4, 27, 22, 00, 00)
-        end = datetime(2024, 4, 28, 21, 59, 59)
-        event = {"dates": {"start": start, "end": end}}
+        start = datetime(2024, 4, 27, 18, 30, 00)
+        end = datetime(2024, 4, 28, 18, 29, 59)
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "ALL DAY 27/04/2024")
+        self.assertEqual(result, "ALL DAY 28/04/2024")
 
     def test_same_start_end(self):
         start = datetime(2024, 4, 1, 14, 45)
         end = datetime(2024, 4, 1, 14, 45)
-        event = {"dates": {"start": start, "end": end}}
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "14:45 01/04/2024")
+        self.assertEqual(result, "20:15 01/04/2024")
 
     def test_dates_same_and_different_time(self):
-        start = datetime(2024, 5, 28, 6, 00, 00)
-        end = datetime(2024, 5, 28, 5, 00, 00)
-        event = {"dates": {"start": start, "end": end}}
+        start = datetime(2024, 5, 28, 5, 00, 00)
+        end = datetime(2024, 5, 28, 6, 00, 00)
+        event = {"dates": {"start": start, "end": end, "tz": "Asia/Calcutta"}}
         result = get_event_formatted_dates(event)
-        self.assertEqual(result, "06:00 - 05:00, 28/05/2024")
+        self.assertEqual(result, "10:30 - 11:30, 28/05/2024")
 
 
 class TestDateRangeFunctions(TestCase):
