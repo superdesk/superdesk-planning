a3111bafcd92d31b46a00fef5a3c4dd8eedfda86
Petr Jasek <jasekpetr@gmail.com>
2024-06-18 13:07:46 +0200

    release 2.7.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ package_data = {
 
 setup(
     name="superdesk-planning",
-    version="2.7.0-rc8",
+    version="2.7.0",
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     package_dir={"": "server"},
