112e4ab5d9e01574c0b23a991f41fdbb565863f5
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-07-19 14:05:41 +0300

    Fix lint

--- client/planning-extension/src/extension.ts
+++ client/planning-extension/src/extension.ts
@@ -128,7 +128,7 @@ const extension: IExtension = {
                         },
                     },
                 },
-                notifications: {'assignment:notification': {type: "email"}},
+                notifications: {'assignment:notification': {type: 'email'}},
                 globalMenuHorizontal: displayTopbarWidget ? [AssignmentsList] : [],
             },
         };
