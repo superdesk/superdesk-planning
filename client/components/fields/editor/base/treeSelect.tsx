dfe26a9782a0e89e05e87f62e1b06cf4bb2d7e2f
devketanpro <devketanpro11@gmail.com>
2024-08-16 09:22:29 +0530

    handle none scheme

--- client/components/fields/editor/base/treeSelect.tsx
+++ client/components/fields/editor/base/treeSelect.tsx
@@ -35,7 +35,7 @@ export class EditorFieldTreeSelect<T> extends React.PureComponent<IEditorFieldTr
             this.props.item,
             this.props.field,
             this.props.defaultValue,
-        )?.filter((value) => value.scheme != this.props.scheme);
+        )?.filter((value) => value?.scheme != this.props.scheme);
         let newValues = this.props.valueAsString ?
             values.map((item) => this.props.getId(item)) :
             values;
@@ -58,7 +58,7 @@ export class EditorFieldTreeSelect<T> extends React.PureComponent<IEditorFieldTr
             values = [values];
         }
 
-        values = values?.filter((value) => this.props.scheme == null || value.scheme === this.props.scheme);
+        values = values?.filter((value) => this.props.scheme == null || value?.scheme === this.props.scheme);
 
         if (this.props.valueAsString) {
             viewValues = options
