9012e413e03671ec53cd687b5d5709c1555f091a
Konstantin Markov <maga.ot.oz@protonmail.com>
2024-10-09 15:01:42 +0300

    Fix import

--- client/planning-extension/src/extension_bridge.ts
+++ client/planning-extension/src/extension_bridge.ts
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {IArticle, IVocabularyItem} from 'superdesk-api';
 import {IAssignmentItem, IEditorFieldProps, IPlanningAppState, IPlanningItem} from '../../interfaces';
-import {IArticle} from 'superdesk-api';
 
 interface IEditorFieldVocabularyProps extends IEditorFieldProps {
     options: Array<any>;
