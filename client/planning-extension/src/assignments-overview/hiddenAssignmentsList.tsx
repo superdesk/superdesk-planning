bd6bf095828fd10a1d5f823abebcfbb926d706cc
dzonidoo <nikola71092@gmail.com>
2024-10-24 13:01:33 +0200

    changes after review part II

--- client/planning-extension/src/assignments-overview/hiddenAssignmentsList.tsx
+++ client/planning-extension/src/assignments-overview/hiddenAssignmentsList.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {ISuperdeskQuery, IUser} from 'superdesk-api';
-import {IAssignmentItem} from '../../../interfaces';
+import {IUser} from 'superdesk-api';
+import {getAssignmentsQuery} from '.';
+import {ASSIGNMENT_STATE, IAssignmentItem} from '../../../interfaces';
 import {superdesk} from '../superdesk';
 const {getLiveQueryHOC} = superdesk.components;
 const LiveAssignmentsHOC = getLiveQueryHOC<IAssignmentItem>();
@@ -8,22 +9,6 @@ const LiveAssignmentsHOC = getLiveQueryHOC<IAssignmentItem>();
 interface IState {
     loading: false;
     currentUser: IUser;
-}
-
-function getAssignmentsQuery(userId: IUser['_id']): ISuperdeskQuery {
-    const query: ISuperdeskQuery = {
-        filter: {
-            $and: [
-                {'assigned_to.user': {$eq: userId}},
-                {'assigned_to.state': {$in: ['assigned']}},
-            ],
-        },
-        sort: [{'planning.scheduled': 'asc'}],
-        page: 1,
-        max_results: 100,
-    };
-
-    return query;
 }
 
 export class HiddenAssignmentsList extends React.PureComponent<{}, {loading: true} | IState> {
@@ -51,20 +36,23 @@ export class HiddenAssignmentsList extends React.PureComponent<{}, {loading: tru
         const {currentUser} = this.state;
 
         return (
-                <LiveAssignmentsHOC resource="assignments" query={getAssignmentsQuery(currentUser._id)}>
-                    {
-                        (data) => {
-                            const itemsCount = data._meta.total;
+            <LiveAssignmentsHOC
+                resource="assignments"
+                query={getAssignmentsQuery(currentUser._id, [ASSIGNMENT_STATE.ASSIGNED])}
+            >
+                {
+                    (data) => {
+                        const itemsCount = data._meta.total;
 
-                            superdesk.dispatchEvent(
-                                'menuItemBadgeValueChange',
-                                {menuId: 'MENU_ITEM_PLANNING_ASSIGNMENTS', badgeValue: itemsCount.toString()},
-                            );
+                        superdesk.dispatchEvent(
+                            'menuItemBadgeValueChange',
+                            {menuId: 'MENU_ITEM_PLANNING_ASSIGNMENTS', badgeValue: itemsCount.toString()},
+                        );
 
-                            return null;
-                        }
+                        return null;
                     }
-                </LiveAssignmentsHOC>
+                }
+            </LiveAssignmentsHOC>
         );
     }
 }
