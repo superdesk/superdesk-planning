9e7f2de7ee5b5eacc27215aced77125a0892f07b
Tomas Kikutis <t.kikutis@gmail.com>
2024-11-26 10:38:32 +0100

    add text fields to profile

--- client/components/planning-editor-standalone/profile.ts
+++ client/components/planning-editor-standalone/profile.ts
@@ -3,12 +3,15 @@ import {OrderedMap} from 'immutable';
 import {
     IAuthoringFieldV2,
     IContentProfileV2,
+    IDateFieldConfig,
+    IDropdownConfigManualSource,
+    IDropdownConfigVocabulary,
     IEditor3Config,
 } from 'superdesk-api';
 import {planningApi, superdeskApi} from '../../superdeskApi';
 import {getEditorFormGroupsFromProfile} from '../../utils/contentProfiles';
 
-function getTextFieldConfig(options: {id: string; label: string}): IAuthoringFieldV2 {
+function getTextFieldConfig(options: {id: string; label: string, required: boolean}): IAuthoringFieldV2 {
     const editor3ConfigWithoutFormatting: IEditor3Config = {
         editorFormat: [],
         minLength: undefined,
@@ -26,7 +29,7 @@ function getTextFieldConfig(options: {id: string; label: string}): IAuthoringFie
         fieldType: 'editor3',
         fieldConfig: {
             ...editor3ConfigWithoutFormatting,
-            required: true,
+            required: options.required,
         },
     };
 
@@ -46,18 +49,156 @@ export function getProfile() {
         header: OrderedMap(),
     };
 
+    const skipped = [];
+
     for (const fieldId of planningFieldIds) {
-        if (fieldId === 'slugline') {
+        const required = planningProfile.schema?.[fieldId]?.required ?? false;
+
+        // TEXT FIELDS
+
+        if (fieldId === 'ednote') {
             profileV2.header = profileV2.header.set(
-                'slugline',
-                getTextFieldConfig({id: 'slugline', label: gettext('Slugline')}),
+                fieldId,
+                getTextFieldConfig({id: fieldId, label: gettext('Ed Note'), required: required}),
             );
+        } else if (fieldId === 'internal_note') {
+            profileV2.header = profileV2.header.set(
+                fieldId,
+                getTextFieldConfig({id: fieldId, label: gettext('Internal Note'), required: required}),
+            );
+        } else if (fieldId === 'name') {
+            profileV2.header = profileV2.header.set(
+                fieldId,
+                getTextFieldConfig({id: fieldId, label: gettext('Name'), required: required}),
+            );
+        } else if (fieldId === 'slugline') {
+            profileV2.header = profileV2.header.set(
+                fieldId,
+                getTextFieldConfig({id: fieldId, label: gettext('Slugline'), required: required}),
+            );
+        } else if (fieldId === 'description_text') {
+            profileV2.header = profileV2.header.set(
+                fieldId,
+                getTextFieldConfig({id: fieldId, label: gettext('Description'), required: required}),
+            );
+        } else if (fieldId === 'headline') {
+            profileV2.header = profileV2.header.set(
+                fieldId,
+                getTextFieldConfig({id: fieldId, label: gettext('Headline'), required: required}),
+            );
+        }
+
+        // else if (fieldId === 'language') {
+        //     const languageFieldConfig: IDropdownConfigVocabulary = {
+        //         source: 'vocabulary',
+        //         vocabularyId: 'languages',
+        //         multiple: false,
+        //         width: 100,
+        //     };
+
+        //     const languageField: IAuthoringFieldV2 = {
+        //         id: 'language',
+        //         name: superdeskApi.entities.vocabulary.getAll().get('languages').display_name,
+        //         fieldType: 'dropdown',
+        //         fieldConfig: languageFieldConfig,
+        //     };
+
+        //     profileV2.header = profileV2.header.set(languageField.id, languageField);
+        // } else if (fieldId === 'planning_date') {
+        //     const planningDateConfig: IDateFieldConfig = {
+        //         // placeholder
+        //     };
+
+        //     // PR-TODO: convert to date-time
+        //     const planningDateField: IAuthoringFieldV2 = {
+        //         id: 'planning_date',
+        //         name: gettext('Planning date'),
+        //         fieldType: 'date',
+        //         fieldConfig: planningDateConfig,
+        //     };
+
+        //     profileV2.header = profileV2.header.set(planningDateField.id, planningDateField);
+        // } else if (fieldId == 'description_text') {
+        //     profileV2.header = profileV2.header.set(
+        //         'description_text',
+        //         getTextFieldConfig({id: 'description_text', label: gettext('Description')}),
+        //     );
+        // } else if (fieldId === 'internal_note') {
+        //     profileV2.header = profileV2.header.set(
+        //         'internal_note',
+        //         getTextFieldConfig({id: 'internal_note', label: gettext('Internal note')}),
+        //     );
+        // } else if (fieldId === 'internal_note') {
+        //     profileV2.header = profileV2.header.set(
+        //         'internal_note',
+        //         getTextFieldConfig({id: 'internal_note', label: gettext('Internal note')}),
+        //     );
+        // } else if (fieldId === 'urgency') {
+        //     const vocabulary = superdeskApi.entities.vocabulary.getAll().get('urgency');
+
+        //     // HAS TO BE SYNCED WITH styles/sass/labels.scss
+        //     var defaultUrgencyColors = {
+        //         0: '#cccccc',
+        //         1: '#01405b',
+        //         2: '#005e84',
+        //         3: '#3684a4',
+        //         4: '#64a4bf',
+        //         5: '#a1c6d8',
+        //     };
+
+        //     const urgencyFieldConfig: IDropdownConfigManualSource = {
+        //         source: 'manual-entry',
+        //         type: 'number',
+        //         options: vocabulary.items.map(({name, qcode, color}) => {
+        //             const option: IDropdownConfigManualSource['options'][0] = {
+        //                 id: qcode,
+        //                 label: name,
+        //                 color: color ?? defaultUrgencyColors[name] ?? undefined,
+        //             };
+
+        //             return option;
+        //         }),
+        //         roundCorners: true,
+        //         multiple: false,
+        //     };
+
+        //     const urgencyField: IAuthoringFieldV2 = {
+        //         id: 'urgency',
+        //         name: gettext('Urgency'),
+        //         fieldType: 'dropdown',
+        //         fieldConfig: urgencyFieldConfig,
+        //     };
+
+        //     profileV2.header = profileV2.header.set(urgencyField.id, urgencyField);
+        // } else if (fieldId === 'anpa_category') {
+        //     const vocabulary = superdeskApi.entities.vocabulary.getAll().get('categories');
+        //     const multiple = true;
+
+        //     const categoryFieldConfig: IDropdownConfigVocabulary = {
+        //         source: 'vocabulary',
+        //         vocabularyId: 'categories',
+        //         multiple: multiple,
+        //     };
+
+        //     const categoryField: IAuthoringFieldV2 = {
+        //         id: 'anpa_category',
+        //         name: vocabulary.display_name,
+        //         fieldType: 'dropdown',
+        //         fieldConfig: categoryFieldConfig,
+        //     };
+
+        //     profileV2.header = profileV2.header.set(categoryField.id, categoryField);
+        // }
+        else {
+            skipped.push(fieldId);
         }
     }
 
     profileV2.header.forEach((item) => {
         item.fieldConfig.width = 100;
     });
+
+    console.log('skipped -------------------------------- ', skipped, superdeskApi.entities.vocabulary.getAll());
 
     return profileV2;
 }
