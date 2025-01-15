4415d89a8c7cf08f7bb0d848da7bcce84f6fd959
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-01-15 12:36:17 +0200

    Fix date time, update client core

--- client/components/editor-standalone/field-definitions/date-time-config.ts
+++ client/components/editor-standalone/field-definitions/date-time-config.ts
@@ -1,19 +1,42 @@
 import {IAuthoringFieldV2, IDateTimeFieldConfig} from 'superdesk-api';
+import {superdeskApi} from '../../../superdeskApi';
+import {IFieldDefinition} from './interfaces';
+import moment from 'moment';
 
-export function getDateTimeField(options: {id: string; label: string, required: boolean}): IAuthoringFieldV2 {
-    const config: IDateTimeFieldConfig = {
-        allowSeconds: false,
-    };
+export function getPlanningDateField(): IFieldDefinition {
+    return {
+        fieldId: 'planning_date',
+        getField: ({id, required}) => {
+            const config: IDateTimeFieldConfig = {
+                allowSeconds: false,
+            };
 
-    const field: IAuthoringFieldV2 = {
-        id: options.id,
-        name: options.label,
-        fieldType: 'datetime',
-        fieldConfig: {
-            ...config,
-            required: options.required,
+            const field: IAuthoringFieldV2 = {
+                id: id,
+                name: superdeskApi.localization.gettext('Planning Date'),
+                fieldType: 'datetime',
+                fieldConfig: {
+                    ...config,
+                    required: required,
+                },
+            };
+
+            return field;
         },
-    };
+        storageAdapterPlanning: {
+            retrieveStoredValue: (item, fieldId) => {
+                if (typeof item[fieldId] === 'object') {
+                    return (item[fieldId] as moment.Moment).toString();
+                }
 
-    return field;
+                return item[fieldId];
+            },
+            storeValue: (item, operationalValue) => {
+                return {
+                    ...item,
+                    planning_date: operationalValue ? new Date(operationalValue) : null,
+                };
+            }
+        }
+    };
 }
