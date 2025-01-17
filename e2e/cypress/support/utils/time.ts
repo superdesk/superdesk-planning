3117d9c5b5117d51b3e08db679c208c7999a7404
Petr Jasek <jasekpetr@gmail.com>
2025-01-17 18:03:10 +0100

    revert changes

--- e2e/cypress/support/utils/time.ts
+++ e2e/cypress/support/utils/time.ts
@@ -29,13 +29,16 @@ export function getStartOfNextWeek(): moment.Moment {
 
 export const getDateStringFor = {
     today: () => moment()
+        .set({hour: 0})
         .utc()
         .format('YYYY-MM-DD'),
     yesterday: () => moment()
+        .set({hour: 0})
         .utc()
         .subtract(1, 'd')
         .format('YYYY-MM-DD'),
     tomorrow: () => moment()
+        .set({hour: 0})
         .utc()
         .add(1, 'd')
         .format('YYYY-MM-DD'),
@@ -45,8 +48,8 @@ export const getDateStringFor = {
 
 export function getTimeStringForHour(hour: number): string {
     return moment()
-        .utc()
         .set({hour: hour})
+        .utc()
         .format('THH:00:00+0000');
 }
 
--- e2e/server/gunicorn_config.py
+++ e2e/server/gunicorn_config.py
@@ -3,7 +3,7 @@ import os
 bind = '0.0.0.0:5000'
 workers = 3
 
-loglevel = 'info'
+loglevel = 'warning'
 
 reload = 'SUPERDESK_RELOAD' in os.environ
 timeout = int(os.environ.get('WEB_TIMEOUT', 500))
