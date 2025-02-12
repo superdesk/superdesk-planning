990895dfc6cec0cf8f6ca1f2f68990b9cf6a2f74
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-02-12 13:26:23 +0200

    Update empty states (#2200)

--- client/components/Coverages/CoverageArrayInput.tsx
+++ client/components/Coverages/CoverageArrayInput.tsx
@@ -23,7 +23,6 @@ import {InputArray} from '../UI/Form';
 import {CoverageEditor} from './CoverageEditor';
 import {CoverageAddButton} from './CoverageAddButton';
 import planningActions from '../../actions/planning/api';
-import {EmptyState} from 'superdesk-ui-framework/react';
 
 
 interface IOwnProps {
@@ -203,16 +202,6 @@ class CoverageArrayInputComponent extends React.Component<IProps, IState> {
                 value={value}
                 coverages={value}
                 onChange={onChange}
-                emptyStateElement={(
-                    <EmptyState
-                        title={superdeskApi.localization.gettext('No coverages have been added')}
-                        description={
-                            superdeskApi.localization.gettext('To add some, click the plus icon')
-                        }
-                        illustration="2"
-                        size="small"
-                    />
-                )}
                 addButtonText={addButtonText}
                 addButtonComponent={CoverageAddButton}
                 addButtonProps={{
