90217a47f8d8ca87db7f9aa9afee6c11bd257eb0
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-02-17 08:52:37 +0200

    Add tooltip

--- client/components/fields/editor/AddCoverageToWorkflow.tsx
+++ client/components/fields/editor/AddCoverageToWorkflow.tsx
@@ -3,18 +3,24 @@ import {IEditorFieldProps} from '../../../interfaces';
 import {EditorFieldToggle} from './base/toggle';
 
 import {superdeskApi} from '../../../superdeskApi';
+import {Tooltip} from '@sourcefabric/common';
 
 export class EditorFieldAddCoverageToWorkflow extends React.PureComponent<IEditorFieldProps> {
     render() {
         const {gettext} = superdeskApi.localization;
 
         return (
-            <EditorFieldToggle
-                {...this.props}
-                field="add_coverage_to_workflow"
-                label={gettext('Add to workflow')}
-                defaultValue={false}
-            />
+            <Tooltip
+                disabled={this.props.disabled === false}
+                content={superdeskApi.localization.gettext('You haven\'t saved the coverage or assigned it')}
+            >
+                <EditorFieldToggle
+                    {...this.props}
+                    field="add_coverage_to_workflow"
+                    label={gettext('Add to workflow')}
+                    defaultValue={false}
+                />
+            </Tooltip>
         );
     }
 }
