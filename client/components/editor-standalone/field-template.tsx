2049a9681e7da836489432b4d6f0193a9efc3d64
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-03-21 12:28:20 +0200

    Remove label for event dates

--- client/components/editor-standalone/field-template.tsx
+++ client/components/editor-standalone/field-template.tsx
@@ -11,43 +11,52 @@ export class FieldTemplate extends React.PureComponent<IPropsAuthoringFieldTempl
         const {field, input, validationError, miniToolbar} = this.props;
         const compactMode = miniToolbar == null || (field.fieldConfig as IEditor3Config).compact;
 
-        const labelJsx = compactMode
-            ? (
-                <div
-                    style={{
-                        color: 'var(--color-label-text)',
-                        fontSize: '1.1rem',
-                        textTransform: 'uppercase',
-                        fontWeight: 500,
-                        letterSpacing: 0.08,
-                    }}
-                >
-                    <Spacer h gap="4" justifyContent="start" noWrap>
-                        {field.name}
-                        {field.fieldConfig.required && requiredIndicator}
-                    </Spacer>
-                </div>
-            )
-            : (
-                <Spacer h gap="4" justifyContent="space-between" noWrap>
-                    <Spacer h gap="4" justifyContent="start" noWrap>
-                        <div
-                            style={{
-                                fontSize: '1.6rem',
-                                fontWeight: 'bold',
-                                color: 'var(--color-text)',
-                            }}
-                        >
-                            {field.name}
-                        </div>
-                        {field.fieldConfig.required && requiredIndicator}
-                    </Spacer>
 
-                    <div>
-                        {miniToolbar}
+        const labelJsx = (() => {
+            if (field.id === 'dates') {
+                return null;
+            }
+
+            if (compactMode) {
+                return (
+                    <div
+                        style={{
+                            color: 'var(--color-label-text)',
+                            fontSize: '1.1rem',
+                            textTransform: 'uppercase',
+                            fontWeight: 500,
+                            letterSpacing: 0.08,
+                        }}
+                    >
+                        <Spacer h gap="4" justifyContent="start" noWrap>
+                            {field.name}
+                            {field.fieldConfig.required && requiredIndicator}
+                        </Spacer>
                     </div>
-                </Spacer>
-            );
+                );
+            } else {
+                return (
+                    <Spacer h gap="4" justifyContent="space-between" noWrap>
+                        <Spacer h gap="4" justifyContent="start" noWrap>
+                            <div
+                                style={{
+                                    fontSize: '1.6rem',
+                                    fontWeight: 'bold',
+                                    color: 'var(--color-text)',
+                                }}
+                            >
+                                {field.name}
+                            </div>
+                            {field.fieldConfig.required && requiredIndicator}
+                        </Spacer>
+
+                        <div>
+                            {miniToolbar}
+                        </div>
+                    </Spacer>
+                );
+            }
+        })();
 
         if (compactMode) {
             return (
