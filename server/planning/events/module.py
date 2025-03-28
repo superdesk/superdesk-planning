495c815933084353c98cf93022acda991db2051e
Mark Pittaway <mark.pittaway@gmail.com>
2025-03-28 16:12:54 +1100

    fix EventHistory async mongo index

--- server/planning/events/module.py
+++ server/planning/events/module.py
@@ -40,7 +40,7 @@ events_history_resource_config: ResourceConfig = ResourceConfig(
     mongo=MongoResourceConfig(
         indexes=[
             MongoIndexOptions(
-                name="event_id",
+                name="event_id_1",
                 keys=[("event_id", 1)],
                 unique=False,
             ),
