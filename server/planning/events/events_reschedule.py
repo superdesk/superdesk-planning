d965a2b9df636746439aa63055100559021c14d2
Brian Mwangi <mwangikabiru21@gmail.com>
2025-04-01 15:21:45 +0300

    Converted PlanningRescheduleService to use AsyncBaseService

--- server/planning/events/events_reschedule.py
+++ server/planning/events/events_reschedule.py
@@ -73,14 +73,14 @@ def mark_event_rescheduled(updates: dict[str, Any], reason: str, keep_dates: boo
 
 
 async def reschedule_event_plannings(original: dict[str, Any], reason: str, plans=None, state=None):
-    # TODO-ASYNC: Convert to use async services when `planning_reschedule` and `planning_history` are converted to async
+    # TODO-ASYNC: Convert to use async services `planning_history` are converted to async
     planning_cancel_service = get_resource_service("planning_cancel")
     planning_reschedule_service = get_resource_service("planning_reschedule")
 
     plan_updates = {"reason": reason, "state": state}
     for plan in plans or get_related_planning_for_events([original[ID_FIELD]], "primary"):
         if plan.get("state") != WORKFLOW_STATE.CANCELLED:
-            updated_plan = planning_reschedule_service.patch(plan[ID_FIELD], plan_updates)
+            updated_plan = await planning_reschedule_service.patch_async(plan[ID_FIELD], plan_updates)
             get_resource_service("planning_history").on_reschedule(updated_plan, plan)
             if len(plan.get("coverages", [])) > 0:
                 await planning_cancel_service.update_async(
