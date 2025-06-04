1eecc5b35fc0c77f78037f8945d16ecbbfb56fa9
Mark Pittaway <mark.pittaway@gmail.com>
2025-06-04 15:05:48 +1000

    Fix FeaturedPlanning ID generation

--- server/planning/types/planning_featured.py
+++ server/planning/types/planning_featured.py
@@ -1,14 +1,21 @@
 from datetime import datetime
-from pydantic import Field
-from typing import Annotated
+from pydantic import Field, model_validator
+from typing import Annotated, Union
 
-from .base import BasePlanningModel
-from superdesk.utc import utcnow
+from eve_elastic.elastic import parse_date
+
+from superdesk.utc import utcnow, utc_to_local
+from superdesk.core import get_config
 from superdesk.core.resources import fields
 from superdesk.core.resources.validators import validate_data_relation_async
 
+from .base import BasePlanningModel
+
+ID_DATE_FORMAT = "%Y%m%d"
+
 
 class PlanningFeaturedResourceModel(BasePlanningModel):
+    id: Annotated[str, Field(alias="_id")]
     date: datetime = Field(default_factory=utcnow)
     items: list[str] = Field(default_factory=list)
     tz: str | None = None
@@ -20,3 +27,20 @@ class PlanningFeaturedResourceModel(BasePlanningModel):
     item_type: str = Field(
         alias="type", default="planning_featured", description="Item type used by superdesk publishing"
     )
+
+    @model_validator(mode="before")
+    @classmethod
+    def set_id(
+        cls, values: Union[dict, "PlanningFeaturedResourceModel"]
+    ) -> Union[dict, "PlanningFeaturedResourceModel"]:
+        values.setdefault("tz", get_config(str, "DEFAULT_TIMEZONE"))
+
+        if not values.get("date"):
+            values["date"] = utcnow()
+        elif isinstance(values["date"], str):
+            values["date"] = parse_date(values["date"])
+
+        # print(values)
+        date = utc_to_local(values["tz"], values["date"])
+        values["_id"] = date.strftime(ID_DATE_FORMAT)
+        return values
