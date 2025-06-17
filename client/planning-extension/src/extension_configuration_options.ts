5dde8c1a800f61ec9f5f657b7a763baa73f5918c
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-06-17 12:01:59 +0300

    Add sorting config (#2359)

--- client/planning-extension/src/extension_configuration_options.ts
+++ client/planning-extension/src/extension_configuration_options.ts
@@ -1,4 +1,13 @@
+import {IPlanningItem} from '../../interfaces';
+
 // It has to be importable from the main superdesk repo.
 export interface IPlanningExtensionConfigurationOptions {
     assignmentsTopBarWidget?: boolean;
+
+    /**
+     * Custom comparison function for sorting planning items
+     *
+     * Orders elements in the planning only list view.
+     */
+    comparePlanningItems?: (a: IPlanningItem, b: IPlanningItem) => number;
 }
