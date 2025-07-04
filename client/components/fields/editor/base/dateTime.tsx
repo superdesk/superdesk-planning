59c6d4c09ad7a0f8afee2a801157dbf9b73b057b
Brian Mwangi <mwangikabiru21@gmail.com>
2025-07-04 12:00:29 +0300

    Fix in EditorFieldDateTime without shift

--- client/components/fields/editor/base/dateTime.tsx
+++ client/components/fields/editor/base/dateTime.tsx
@@ -54,7 +54,11 @@ export class EditorFieldDateTime extends React.PureComponent<IProps> {
         let momentValue : moment.Moment;
 
         if (value != null) {
-            momentValue = this.props.allDay ? moment.utc(value) : moment(value);
+            if (this.props.allDay) {
+                momentValue = moment.tz(moment(value).format('YYYY-MM-DD'), this.props.remoteTimeZone || moment.tz.guess());
+            } else {
+                momentValue = moment(value);
+            }
         } else {
             momentValue = undefined;
         }
