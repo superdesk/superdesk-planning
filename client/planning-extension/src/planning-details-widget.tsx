264673746864a8912c381c7263e22d9d813f9be0
Ketan <73937490+devketanpro@users.noreply.github.com>
2025-07-17 16:10:41 +0530

    As an editor using the Planning Details Widget, I want to have certain actions available concerning a related Planning item [STT-128] (#2399)
    * As an editor using the Planning Details Widget, I want to have certain actions available concerning a related Planning item [STT-128]
    
    * refactor code
    
    * update package-lock
    
    * undo changes
    
    * Fix import
    
    * update package-lock
    
    * Revert 'update package-lock'
    
    * Fix build
    
    * Remove volta pin
    
    ---------
    
    Co-authored-by: Konstantin Markov <maga.ot.oz@protonmail.com>

--- client/planning-extension/src/planning-details-widget.tsx
+++ client/planning-extension/src/planning-details-widget.tsx
@@ -2,20 +2,83 @@ import * as React from 'react';
 import {IArticleSideWidgetComponentType} from 'superdesk-api';
 import {superdesk} from './superdesk';
 import {extensionBridge} from './extension_bridge';
-
+import {Button, Spinner} from 'superdesk-ui-framework/react';
+const {editPlanningInNewTab} = extensionBridge.planning;
 const {AuthoringWidgetLayout, AuthoringWidgetHeading} = superdesk.components;
 const {gettext} = superdesk.localization;
 
 export const PLANNING_DETAILS_WIDGET_ID = 'planning_details';
 export const PLANNING_DETAILS_WIDGET_LABEL = gettext('Planning Details');
 
-export class PlanningDetailsWidget extends React.PureComponent<IArticleSideWidgetComponentType> {
-    render() {
-        const PlanningDetailsBody = extensionBridge.ui.components.PlanningDetailsWidget;
+interface IState {
+    loading: boolean;
+    planningId: string | null;
+}
 
-        if (this.props.article.assignment_id == null) {
-            return null;
+export class PlanningDetailsWidget extends React.PureComponent<IArticleSideWidgetComponentType, IState> {
+    constructor(props: IArticleSideWidgetComponentType) {
+        super(props);
+
+        this.state = {
+            loading: props.article.assignment_id != null,
+            planningId: null,
+        };
+    }
+
+    componentDidMount() {
+        const {assignment_id} = this.props.article;
+
+        if (assignment_id != null) {
+            extensionBridge.planning.getItemPlanningInfo({assignment_id})
+                .then((planning) => {
+                    this.setState({
+                        planningId: planning._id,
+                        loading: false,
+                    });
+                })
+                .catch(() => {
+                    this.setState({loading: false});
+                });
         }
+    }
+
+    dispatchAddToPlanning = () => {
+        window.dispatchEvent(new CustomEvent('planning:addToPlanning', {
+            detail: this.props.article
+        }));
+    };
+
+    dispatchFulfilAssignment = () => {
+        window.dispatchEvent(new CustomEvent('planning:fulfilassignment', {
+            detail: {item: this.props.article}
+        }));
+    };
+
+    dispatchUnlinkCoverage = () => {
+        superdesk.entities.article.get(this.props.article._id).then((_item) => {
+            window.dispatchEvent(new CustomEvent('planning:unlinkfromcoverage', {
+                detail: {item: _item}
+            }));
+        });
+    };
+
+    openPlanningEditor = () => {
+        const {planningId} = this.state;
+
+        if (planningId != null) {
+            editPlanningInNewTab(planningId);
+        }
+    };
+
+    render() {
+        const {assignment_id} = this.props.article;
+        const {loading} = this.state;
+
+        if (loading) {
+            return <Spinner size="large" />;
+        }
+
+        const PlanningDetailsBody = extensionBridge.ui.components.PlanningDetailsWidget;
 
         return (
             <AuthoringWidgetLayout
@@ -27,7 +90,44 @@ export class PlanningDetailsWidget extends React.PureComponent<IArticleSideWidge
                     />
                 )}
                 body={(
-                    <PlanningDetailsBody item={{assignment_id: this.props.article.assignment_id}} />
+                    <div>
+                        {assignment_id != null ? (
+                            <>
+                                <PlanningDetailsBody item={{assignment_id}} />
+                                <div className="sd-margin--2">
+                                    <Button
+                                        text={gettext('Edit Planning')}
+                                        icon="edit"
+                                        onClick={this.openPlanningEditor}
+                                        expand
+                                    />
+                                    <div className="sd-margin-t--1" />
+                                    <Button
+                                        text={gettext('Unlink as Coverage')}
+                                        icon="cut"
+                                        onClick={this.dispatchUnlinkCoverage}
+                                        expand
+                                    />
+                                </div>
+                            </>
+                        ) : (
+                            <div className="sd-margin--2">
+                                <Button
+                                    text={gettext('Add to Planning')}
+                                    icon="calendar-list"
+                                    onClick={this.dispatchAddToPlanning}
+                                    expand
+                                />
+                                <div className="sd-margin-t--1" />
+                                <Button
+                                    text={gettext('Fulfil Assignment')}
+                                    icon="bolt"
+                                    onClick={this.dispatchFulfilAssignment}
+                                    expand
+                                />
+                            </div>
+                        )}
+                    </div>
                 )}
             />
         );
