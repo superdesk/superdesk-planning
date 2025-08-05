8dfadc7a6c067ef6d44496bf2f5c7bb218743652
Brian Mwangi <mwangikabiru21@gmail.com>
2025-08-05 13:33:48 +0300

    Fix location details error in the embedded events editor

--- client/planning-extension/src/authoring-react-fields/location/editor.tsx
+++ client/planning-extension/src/authoring-react-fields/location/editor.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {IEditorComponentProps} from 'superdesk-api';
 import {ILocationFieldConfig, ILocationFieldUserPreferences, ILocationValueOperational} from './interfaces';
 import {extensionBridge} from '../../extension_bridge';
+import {cloneDeep, set} from 'lodash';
 
 type IProps = IEditorComponentProps<ILocationValueOperational, ILocationFieldConfig, ILocationFieldUserPreferences>;
 
@@ -9,25 +10,22 @@ export class Editor extends React.PureComponent<IProps> {
     render() {
         const {EditorFieldLocation} = extensionBridge.editor.fields;
 
-        const convertedValue = (() => {
-            if (this.props.value?.[0] != null) {
-                return this.props.value[0];
-            }
-
-            return this.props.value;
-        })();
-
         return (
             <EditorFieldLocation
                 field="location"
                 enableExternalSearch
-                item={{
-                    location: convertedValue,
-                }}
+                item={{location: this.props.value ?? {}}}
                 required={this.props.config.required}
                 disabled={this.props.config.readOnly}
-                onChange={(_field, value) => {
-                    this.props.onChange(value);
+                onChange={(field: string, value: any) => {
+                    if (field === 'location') {
+                        this.props.onChange(value);
+                    } else if (field === 'location.details') {
+                        const valueCopy = cloneDeep(this.props.value ?? {});
+
+                        set(valueCopy, 'details', value);
+                        this.props.onChange(valueCopy);
+                    }
                 }}
             />
         );
