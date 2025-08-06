949f7c7886973d17e4d77f52fb0e3e912d3de0ac
Brian Mwangi <mwangikabiru21@gmail.com>
2025-08-06 21:45:38 +0300

    Fix location details error in the embedded events editor

--- client/planning-extension/src/authoring-react-fields/location/editor.tsx
+++ client/planning-extension/src/authoring-react-fields/location/editor.tsx
@@ -2,32 +2,36 @@ import * as React from 'react';
 import {IEditorComponentProps} from 'superdesk-api';
 import {ILocationFieldConfig, ILocationFieldUserPreferences, ILocationValueOperational} from './interfaces';
 import {extensionBridge} from '../../extension_bridge';
+import {cloneDeep, set} from 'lodash';
 
 type IProps = IEditorComponentProps<ILocationValueOperational, ILocationFieldConfig, ILocationFieldUserPreferences>;
 
 export class Editor extends React.PureComponent<IProps> {
     render() {
         const {EditorFieldLocation} = extensionBridge.editor.fields;
-
-        const convertedValue = (() => {
-            if (this.props.value?.[0] != null) {
-                return this.props.value[0];
-            }
-
-            return this.props.value;
-        })();
+        const location = Array.isArray(this.props.value) ? this.props.value[0] : this.props.value;
 
         return (
             <EditorFieldLocation
                 field="location"
                 enableExternalSearch
-                item={{
-                    location: convertedValue,
-                }}
+                item={{location: location ?? {}}}
                 required={this.props.config.required}
                 disabled={this.props.config.readOnly}
-                onChange={(_field, value) => {
-                    this.props.onChange(value);
+                onChange={(field: string, value: any) => {
+                    const currentDetails = location?.details;
+                    const valueCopy = {location: cloneDeep(location ?? {})};
+
+                    set(valueCopy, field, value);
+
+                    // Preserve location.details if a new location was selected without details
+                    if (field === 'location' && currentDetails != null && value?.details == null) {
+                        valueCopy.location = {
+                            ...valueCopy.location,
+                            details: currentDetails
+                        };
+                    }
+                    this.props.onChange([valueCopy.location]);
                 }}
             />
         );
