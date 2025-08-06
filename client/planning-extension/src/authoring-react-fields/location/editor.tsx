769ca01c837621700f471a4617c3b1de7e6ae9b9
Brian Mwangi <mwangikabiru21@gmail.com>
2025-08-06 14:18:16 +0300

    Fix typescript error

--- client/planning-extension/src/authoring-react-fields/location/editor.tsx
+++ client/planning-extension/src/authoring-react-fields/location/editor.tsx
@@ -9,29 +9,27 @@ type IProps = IEditorComponentProps<ILocationValueOperational, ILocationFieldCon
 export class Editor extends React.PureComponent<IProps> {
     render() {
         const {EditorFieldLocation} = extensionBridge.editor.fields;
+        const location = Array.isArray(this.props.value) ? this.props.value[0] : this.props.value;
 
         return (
             <EditorFieldLocation
                 field="location"
                 enableExternalSearch
-                item={{location: this.props.value ?? {}}}
+                item={{location: location ?? {}}}
                 required={this.props.config.required}
                 disabled={this.props.config.readOnly}
                 onChange={(field: string, value: any) => {
-                    const currentDetails = this.props.value?.details;
-                    const valueCopy = {location: cloneDeep(this.props.value ?? {})};
+                    const currentDetails = location?.details;
+                    const valueCopy = {location: cloneDeep(location ?? {})};
 
                     set(valueCopy, field, value);
 
                     // Preserve location.details if a new location was selected without details
                     if (field === 'location' && currentDetails && !value?.details) {
-                        valueCopy.location = {
-                            ...valueCopy.location,
-                            details: currentDetails
-                        };
+                        set(valueCopy, 'location.details', currentDetails);
                     }
 
-                    this.props.onChange(valueCopy.location);
+                    this.props.onChange([valueCopy.location]);
                 }}
             />
         );
