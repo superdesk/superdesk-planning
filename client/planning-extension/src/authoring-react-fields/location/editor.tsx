16d87e25e618d3f131793e763dd6f61b78031b50
Brian Mwangi <mwangikabiru21@gmail.com>
2025-08-07 11:08:02 +0300

    Rewrite location onchange handler

--- client/planning-extension/src/authoring-react-fields/location/editor.tsx
+++ client/planning-extension/src/authoring-react-fields/location/editor.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {IEditorComponentProps} from 'superdesk-api';
 import {ILocationFieldConfig, ILocationFieldUserPreferences, ILocationValueOperational} from './interfaces';
+import {ILocation} from '../../../../interfaces';
 import {extensionBridge} from '../../extension_bridge';
 import {cloneDeep, set} from 'lodash';
 
@@ -9,30 +10,40 @@ type IProps = IEditorComponentProps<ILocationValueOperational, ILocationFieldCon
 export class Editor extends React.PureComponent<IProps> {
     render() {
         const {EditorFieldLocation} = extensionBridge.editor.fields;
-        const location = Array.isArray(this.props.value) ? this.props.value[0] : this.props.value;
+        const location: ILocation | null =
+            Array.isArray(this.props.value)
+                ? this.props.value?.[0] ?? null
+                : (this.props.value as any) ?? null;
+
+        const handleChange = (field: string, value: any) => {
+            const previousDetails = location?.details;
+            const nextValue: {location: ILocation | null} = {
+                location: cloneDeep(location ?? ({} as ILocation)),
+            };
+
+            set(nextValue, field, value);
+
+            if (field === 'location' && previousDetails != null) {
+                const newDetails = (value as ILocation | null)?.details;
+                const hasNewDetails = Array.isArray(newDetails) && newDetails.length > 0;
+
+                if (hasNewDetails === false) {
+                    nextValue.location = nextValue.location ?? ({} as ILocation);
+                    nextValue.location.details = previousDetails;
+                }
+            }
+
+            this.props.onChange([nextValue.location] as ILocationValueOperational);
+        };
 
         return (
             <EditorFieldLocation
                 field="location"
                 enableExternalSearch
-                item={{location: location ?? {}}}
+                item={{location}}
                 required={this.props.config.required}
                 disabled={this.props.config.readOnly}
-                onChange={(field: string, value: any) => {
-                    const currentDetails = location?.details;
-                    const valueCopy = {location: cloneDeep(location ?? {})};
-
-                    set(valueCopy, field, value);
-
-                    // Preserve location.details if a new location was selected without details
-                    if (field === 'location' && currentDetails != null && value?.details == null) {
-                        valueCopy.location = {
-                            ...valueCopy.location,
-                            details: currentDetails
-                        };
-                    }
-                    this.props.onChange([valueCopy.location]);
-                }}
+                onChange={handleChange}
             />
         );
     }
