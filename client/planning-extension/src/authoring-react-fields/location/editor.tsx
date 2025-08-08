63fb191945a7a9ca0cfd8958499bd623612f6b59
Brian Mwangi <mwangikabiru21@gmail.com>
2025-08-08 13:07:23 +0300

    [STT-1260] - Fix location details error in the embedded events editor (#2441)
    * Fix location details error in the embedded events editor
    
    * Refactored code to remove if/else and hardcoding values
    
    * Fix typescript error
    
    * Revert changes to package-lock.json
    
    * Remove preservation logic for test
    
    * Code refactor
    
    * Fix CI
    
    * Added null check when getting details
    
    * Suggested fixes
    
    * Remove preservation logic for test
    
    * Revert to first implementation for test
    
    * Reverted suggested fixes
    
    * Rewrite location onchange handler
    
    * Suggested fixes
    
    * Suggested fixes
    
    * Final fixes
    
    * Readdded isLocationObject for extra check
    
    * Added comment
    
    * Use immer instead of cloneDeep
    
    * Removed immer
    
    * Added final explicit null check
    
    ---------
    
    Co-authored-by: Konstantin Markov <maga.ot.oz@protonmail.com>

--- client/planning-extension/src/authoring-react-fields/location/editor.tsx
+++ client/planning-extension/src/authoring-react-fields/location/editor.tsx
@@ -1,34 +1,56 @@
 import * as React from 'react';
 import {IEditorComponentProps} from 'superdesk-api';
 import {ILocationFieldConfig, ILocationFieldUserPreferences, ILocationValueOperational} from './interfaces';
+import {ILocation} from '../../../../interfaces';
 import {extensionBridge} from '../../extension_bridge';
+import {set} from 'lodash';
+import {produce} from 'immer';
 
 type IProps = IEditorComponentProps<ILocationValueOperational, ILocationFieldConfig, ILocationFieldUserPreferences>;
+type LocationOrDetailsValue = ILocation | string | null;
+
+const isLocationObject = (v: LocationOrDetailsValue): v is ILocation =>
+    typeof v === 'object' && v !== null;
 
 export class Editor extends React.PureComponent<IProps> {
     render() {
         const {EditorFieldLocation} = extensionBridge.editor.fields;
 
-        const convertedValue = (() => {
-            if (this.props.value?.[0] != null) {
-                return this.props.value[0];
-            }
+        /*
+        * Schema/back-end provide `location` as an array,
+        * but the editor supports only a single entry.
+        * If we receive an array, pick element 0; otherwise
+        * treat the value as the lone ILocation.
+        */
+        const location: ILocation | null = this.props.value?.[0] ?? this.props.value;
 
-            return this.props.value;
-        })();
+        const handleChange = (field: string, value: LocationOrDetailsValue) => {
+            const previousDetails = location?.details;
+            const nextValue = produce({location}, (draft) => {
+                set(draft, field, value);
+
+                if (field === 'location' && previousDetails != null) {
+                    const newDetails = isLocationObject(value) ? value.details : undefined;
+                    const hasNewDetails = (newDetails ?? []).length > 0;
+
+                    if (!hasNewDetails) {
+                        draft.location = draft.location ?? ({} as ILocation);
+                        draft.location.details = previousDetails;
+                    }
+                }
+            });
+
+            this.props.onChange(nextValue.location != null ? [nextValue.location] : []);
+        };
 
         return (
             <EditorFieldLocation
                 field="location"
                 enableExternalSearch
-                item={{
-                    location: convertedValue,
-                }}
+                item={{location}}
                 required={this.props.config.required}
                 disabled={this.props.config.readOnly}
-                onChange={(_field, value) => {
-                    this.props.onChange(value);
-                }}
+                onChange={handleChange}
             />
         );
     }
