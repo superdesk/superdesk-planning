71d6f28db5076adc101b5385534d4373b1293173
Brian Mwangi <mwangikabiru21@gmail.com>
2025-08-19 11:34:53 +0300

    Added logic to unlink planning items too in the content api

--- server/planning/content_api/resources/event.py
+++ server/planning/content_api/resources/event.py
@@ -20,7 +20,7 @@ from content_api import MONGO_PREFIX, ELASTIC_PREFIX
 from planning.utils import get_related_planning_for_events
 
 from .planning import ContentAPIPlanningService
-from ..types import ContentAPIEventResource
+from ..types import ContentAPIEventResource, ContentAPIPlanningResource
 from ..output_formatters import ContentApiEventFormatter
 
 
@@ -44,10 +44,38 @@ class ContentAPIEventService(AsyncResourceService[ContentAPIEventResource]):
         else:
             await self.create([formatted_item])
 
-        # Check for linked planning items to the event and update them as well
-        related_planning_items = get_related_planning_for_events([event_id])
-        for planning_item in related_planning_items:
-            await ContentAPIPlanningService().publish_async(planning_item, subscribers)
+        # Get set of planning items linked to this event from core
+        planning_service = ContentAPIPlanningResource.get_service()
+        expected_plans = get_related_planning_for_events([event_id]) or []
+        expected_plan_ids = {p["_id"] for p in expected_plans}
+
+        # Get what is currently linked in planning content api db
+        cursor = await planning_service.find(
+            {"query": {"bool": {"must": [{"term": {"events.literal": event_id}}]}}},
+            max_results=500,
+            projection=["_id", "events"],
+        )
+        linked_plans = await cursor.to_list_raw()
+        linked_plan_ids = {p["_id"] for p in linked_plans}
+
+        # Unlink stale planning items - so they don't show in "plans" on fetch
+        stale_plan_ids = linked_plan_ids - expected_plan_ids
+        for plan_id in stale_plan_ids:
+            plan_doc = next((p for p in linked_plans if p["_id"] == plan_id), None)
+            if plan_doc is None:
+                plan_doc = await planning_service.find_by_id(plan_id)
+
+            existing_events = plan_doc.get("events") or []
+            updated_events = [ev for ev in existing_events if ev.get("literal") != event_id]
+
+            # Update only when there’s a change
+            if updated_events != existing_events:
+                await planning_service.update(plan_id, {"events": updated_events})
+                await ContentAPIPlanningService().publish_async({"_id": plan_id}, subscribers)
+
+        # Publish all still-linked planning items
+        for plan in expected_plans:
+            await ContentAPIPlanningService().publish_async(plan, subscribers)
 
 
 content_api_event_resource_config: ResourceConfig = ResourceConfig(
