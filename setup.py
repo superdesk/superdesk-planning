ad7e01faa07cfb01f29b2dc49e4f49583f722d93
Mark Pittaway <mark.pittaway@gmail.com>
2025-09-01 14:06:49 +1000

    Release 3.0.0-beta.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ package_data = {
 
 setup(
     name="superdesk-planning",
-    version="3.0.0-dev.0",
+    version="3.0.0-beta.1",
     description=DESCRIPTION,
     long_description=DESCRIPTION,
     package_dir={"": "server"},
