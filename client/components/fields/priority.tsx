9aa2b3baa6886f3332022bc0a48d960d1288e889
Tomas Kikutis <t.kikutis@gmail.com>
2025-09-16 17:55:55 +0200

    priority field

---
+++ client/components/fields/priority.tsx
@@ -0,0 +1,46 @@
+import React, {FunctionComponent} from 'react';
+import {Spacer} from '@sourcefabric/common';
+import {superdeskApi} from '../../superdeskApi';
+import {IFieldsProps} from '../../interfaces';
+import {DEFAULT_PRIORITY_COLORS} from '../../components/editor-standalone/field-definitions/priority-field';
+import {getTextColor} from 'superdesk-ui-framework/react';
+
+export const priority: FunctionComponent<IFieldsProps> = (props) => {
+    const {gettext} = superdeskApi.localization;
+    const {item} = props;
+
+    if (item.priority == null) {
+        return null;
+    }
+
+    const vocabulary = superdeskApi.entities.vocabulary.getAll().get('priority');
+    const vocabularyItem = vocabulary.items.find(({qcode}) => qcode.toString() === item.priority.toString());
+    const label = vocabularyItem == null
+        ? item.priority.toString()
+        : superdeskApi.entities.vocabulary.getVocabularyItemNameTranslated(vocabularyItem);
+
+    const backgroundColor = vocabularyItem.color ?? DEFAULT_PRIORITY_COLORS[item.priority];
+
+    return (
+        <Spacer h gap="4" justifyContent="start" noWrap style={{whiteSpace: 'nowrap', width: 'auto'}}>
+            <span className="sd-list-item__text-label">{gettext('Priority:')}</span>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <div
+                    style={{
+                        display: 'inline-flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '1.2em',
+                        minWidth: '1.2em',
+                        paddingInline: 'var(--space--0-5)',
+                        background: backgroundColor,
+                        color: getTextColor(backgroundColor),
+                        borderRadius: 'var(--b-radius--x-small)',
+                    }}
+                >
+                    {label}
+                </div>
+            </div>
+        </Spacer>
+    );
+};
