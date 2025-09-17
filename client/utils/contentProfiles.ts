5499e1bb16c9dd9362221b5a6d8f775f8cb4770b
Konstantin Markov <maga.ot.oz@protonmail.com>
2025-09-17 18:53:20 +0300

    Fix lint

--- client/utils/contentProfiles.ts
+++ client/utils/contentProfiles.ts
@@ -259,98 +259,98 @@ export function getFieldNameTranslated(field: string): string {
     const {gettext} = superdeskApi.localization;
 
     switch (field) {
-        case 'recurring_rules':
-            return gettext('Recurring Rules');
-        case 'dates':
-            return gettext('Dates');
-        case 'language':
-            return gettext('Language');
-        case 'slugline':
-            return gettext('Slugline');
-        case 'name':
-            return gettext('Name');
-        case 'definition_short':
-            return gettext('Description Short');
-        case 'reference':
-            return gettext('Reference');
-        case 'calendars':
-            return gettext('Calendars');
-        case 'place':
-            return gettext('Place');
-        case 'occur_status':
-            return gettext('Occur Status');
-        case 'location':
-            return gettext('Location');
-        case 'event_contact_info':
-            return gettext('Contacts');
-        case 'anpa_category':
-            return superdeskApi.entities.vocabulary.getVocabulary('categories').display_name ?? gettext('ANPA Category');
-        case 'subject':
-            return gettext('Subject');
-        case 'definition_long':
-            return gettext('Description Long');
-        case 'internal_note':
-            return gettext('Internal Note');
-        case 'ednote':
-            return gettext('Editorial Note');
-        case 'files':
-            return gettext('Files');
-        case 'links':
-            return gettext('Links');
-        case 'related_plannings':
-            return gettext('Related Plannings');
-        case 'planning_date':
-            return gettext('Planning Date');
-        case 'description_text':
-            return gettext('Description Text');
-        case 'agendas':
-            return gettext('Agendas');
-        case 'urgency':
-            return gettext('Urgency');
-        case 'marked_for_not_publication':
-            return gettext('Marked For Not Publication');
-        case 'overide_auto_assign_to_workflow':
-            return gettext('Override Auto Assign to Workflow');
-        case 'associated_event':
-            return gettext('Associated Event');
-        case 'coverages':
-            return gettext('Coverages');
-        case 'headline':
-            return gettext('Headline');
-        case 'g2_content_type':
-            return gettext('Content Type');
-        case 'add_coverage_to_workflow':
-            return gettext('Add Coverage To Workflow');
-        case 'genre':
-            return gettext('Genre');
-        case 'news_coverage_status':
-            return gettext('Coverage Status');
-        case 'scheduled':
-            return gettext('Scheduled');
-        case 'scheduled_updates':
-            return gettext('Scheduled Updates');
-        case 'contact_info':
-            return gettext('Contacts');
-        case 'keyword':
-            return gettext('Keywords');
-        case 'no_content_linking':
-            return gettext('No Content Linking');
-        case 'xmp_file':
-            return gettext('XMP File');
-        case 'registration_details':
-            return gettext('Registration Details');
-        case 'invitation_details':
-            return gettext('Invitation Details');
-        case 'accreditation_info':
-            return gettext('Accreditation Info');
-        case 'accreditation_deadline':
-            return gettext('Accreditation Deadline');
-        case 'priority':
-            return gettext('Priority');
-        case 'related_items':
-            return gettext('Related Articles');
-        case 'multiple_content':
-            return gettext('Multiple Content');
+    case 'recurring_rules':
+        return gettext('Recurring Rules');
+    case 'dates':
+        return gettext('Dates');
+    case 'language':
+        return gettext('Language');
+    case 'slugline':
+        return gettext('Slugline');
+    case 'name':
+        return gettext('Name');
+    case 'definition_short':
+        return gettext('Description Short');
+    case 'reference':
+        return gettext('Reference');
+    case 'calendars':
+        return gettext('Calendars');
+    case 'place':
+        return gettext('Place');
+    case 'occur_status':
+        return gettext('Occur Status');
+    case 'location':
+        return gettext('Location');
+    case 'event_contact_info':
+        return gettext('Contacts');
+    case 'anpa_category':
+        return superdeskApi.entities.vocabulary.getVocabulary('categories').display_name ?? gettext('ANPA Category');
+    case 'subject':
+        return gettext('Subject');
+    case 'definition_long':
+        return gettext('Description Long');
+    case 'internal_note':
+        return gettext('Internal Note');
+    case 'ednote':
+        return gettext('Editorial Note');
+    case 'files':
+        return gettext('Files');
+    case 'links':
+        return gettext('Links');
+    case 'related_plannings':
+        return gettext('Related Plannings');
+    case 'planning_date':
+        return gettext('Planning Date');
+    case 'description_text':
+        return gettext('Description Text');
+    case 'agendas':
+        return gettext('Agendas');
+    case 'urgency':
+        return gettext('Urgency');
+    case 'marked_for_not_publication':
+        return gettext('Marked For Not Publication');
+    case 'overide_auto_assign_to_workflow':
+        return gettext('Override Auto Assign to Workflow');
+    case 'associated_event':
+        return gettext('Associated Event');
+    case 'coverages':
+        return gettext('Coverages');
+    case 'headline':
+        return gettext('Headline');
+    case 'g2_content_type':
+        return gettext('Content Type');
+    case 'add_coverage_to_workflow':
+        return gettext('Add Coverage To Workflow');
+    case 'genre':
+        return gettext('Genre');
+    case 'news_coverage_status':
+        return gettext('Coverage Status');
+    case 'scheduled':
+        return gettext('Scheduled');
+    case 'scheduled_updates':
+        return gettext('Scheduled Updates');
+    case 'contact_info':
+        return gettext('Contacts');
+    case 'keyword':
+        return gettext('Keywords');
+    case 'no_content_linking':
+        return gettext('No Content Linking');
+    case 'xmp_file':
+        return gettext('XMP File');
+    case 'registration_details':
+        return gettext('Registration Details');
+    case 'invitation_details':
+        return gettext('Invitation Details');
+    case 'accreditation_info':
+        return gettext('Accreditation Info');
+    case 'accreditation_deadline':
+        return gettext('Accreditation Deadline');
+    case 'priority':
+        return gettext('Priority');
+    case 'related_items':
+        return gettext('Related Articles');
+    case 'multiple_content':
+        return gettext('Multiple Content');
     }
 
     return field;
