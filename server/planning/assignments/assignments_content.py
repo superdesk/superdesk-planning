bdd8013f8e1824e7cdb21832829808bb92928659
Brian Mwangi <mwangikabiru21@gmail.com>
2025-10-14 10:36:35 +0300

    When creating a new story, inherit ANPA categoty from coverage first, else from the planning item

--- server/planning/assignments/assignments_content.py
+++ server/planning/assignments/assignments_content.py
@@ -94,7 +94,14 @@ async def get_item_from_assignment(assignment, template=None):
 
                 await merge_subject(item, planning)
                 merge_list("place", item, planning)
-                merge_list("anpa_category", item, planning)
+
+                # Inherit anpa_category from coverage first, then fallback to planning item
+                coverage = await get_coverage_for_assignment(assignment) or {}
+                coverage_anpa_category = (coverage.get("planning") or {}).get("anpa_category")
+                if coverage_anpa_category:
+                    item["anpa_category"] = deepcopy(coverage_anpa_category)
+                else:
+                    merge_list("anpa_category", item, planning)
 
             if assignment.get("description_text"):
                 item["abstract"] = "<p>{}</p>".format(assignment["description_text"])
