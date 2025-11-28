f01bfcc62ca1e867eab8cd7bebdc1f1560bbc4d1
Helmy Giacoman <helmygb@gmail.com>
2025-11-28 11:22:10 +0100

    [STT-1491] - Fix pagination infinite loop (#2625)
    * Refactor eventsPlanning reducer to use immer
    
    Replaced lodash-based state updates with immer's produce for immutability in the eventsPlanning reducer. Also updated ListPanel to prevent loading more items when all are loaded and improved setState usage for loading next page.
    
    * Revert reducer as `produce` seems overkill
    
    STT-1491

--- client/reducers/eventsplanning.ts
+++ client/reducers/eventsplanning.ts
@@ -1,4 +1,5 @@
-import {cloneDeep, get, uniq, sortBy} from 'lodash';
+import {get, sortBy} from 'lodash';
+import {produce} from 'immer';
 
 import {IEventsPlanningState, IMainState, ISearchFilter} from '../interfaces';
 import {EVENTS_PLANNING, INIT_STORE, RESET_STORE, MAIN} from '../constants';
@@ -35,27 +36,28 @@ const eventsPlanningReducer = createReducer<IEventsPlanningState>(initialState, 
 
     [INIT_STORE]: () => (initialState),
 
-    [EVENTS_PLANNING.ACTIONS.SET_EVENTS_PLANNING_LIST]: (state, payload) => (
-        {
-            ...state,
-            eventsAndPlanningInList: (payload || []).map((e) => e._id),
-        }
-    ),
-    [EVENTS_PLANNING.ACTIONS.ADD_EVENTS_PLANNING_LIST]: (state, payload) => (
-        {
-            ...state,
-            eventsAndPlanningInList: uniq([
-                ...cloneDeep(state.eventsAndPlanningInList || []),
-                ...(payload || []).map((e) => e._id),
-            ]),
-        }
-    ),
-    [EVENTS_PLANNING.ACTIONS.CLEAR_EVENTS_PLANNING_LIST]: (state, payload) => (
-        {
-            ...state,
-            eventsAndPlanningInList: [],
-        }
-    ),
+    [EVENTS_PLANNING.ACTIONS.SET_EVENTS_PLANNING_LIST]: (state, payload) =>
+        produce(state, (draft) => {
+            draft.eventsAndPlanningInList = (payload || []).map((e) => e._id);
+        }),
+
+    [EVENTS_PLANNING.ACTIONS.ADD_EVENTS_PLANNING_LIST]: (state, payload) =>
+        produce(state, (draft) => {
+            const newIds = (payload || []).map((e) => e._id);
+            const existingIds = new Set(draft.eventsAndPlanningInList || []);
+
+            newIds.forEach((id) => {
+                if (!existingIds.has(id)) {
+                    draft.eventsAndPlanningInList.push(id);
+                }
+            });
+        }),
+
+    [EVENTS_PLANNING.ACTIONS.CLEAR_EVENTS_PLANNING_LIST]: (state, payload) =>
+        produce(state, (draft) => {
+            draft.eventsAndPlanningInList = [];
+        }),
+
     [EVENTS_PLANNING.ACTIONS.SHOW_RELATED_PLANNINGS]: (state, payload) => {
         const eventId = getItemId(payload);
 
@@ -63,32 +65,26 @@ const eventsPlanningReducer = createReducer<IEventsPlanningState>(initialState, 
             return state;
         }
 
-        return {
-            ...state,
-            relatedPlannings: {
-                ...state.relatedPlannings,
-                [eventId]: get(payload, 'planning_ids', []),
-            },
-        };
+        return produce(state, (draft) => {
+            draft.relatedPlannings[eventId] = get(payload, 'planning_ids', []);
+        });
     },
-    [EVENTS_PLANNING.ACTIONS.ADD_OR_REPLACE_EVENTS_PLANNING_FILTER]: (state, payload) => (
-        {
-            ...state,
-            filters: addOrReplaceFilters([...state.filters], payload),
-        }
-    ),
-    [EVENTS_PLANNING.ACTIONS.RECEIVE_EVENTS_PLANNING_FILTERS]: (state, payload) => (
-        {
-            ...state,
-            filters: sortBy([...payload], [(f) => f.name.toLowerCase()]),
-        }
-    ),
-    [EVENTS_PLANNING.ACTIONS.SELECT_EVENTS_PLANNING_FILTER]: (state, payload: ISearchFilter['_id']) => (
-        {
-            ...state,
-            currentFilter: payload === EVENTS_PLANNING.FILTER.ALL_EVENTS_PLANNING ? null : payload,
-        }
-    ),
+
+    [EVENTS_PLANNING.ACTIONS.ADD_OR_REPLACE_EVENTS_PLANNING_FILTER]: (state, payload) =>
+        produce(state, (draft) => {
+            draft.filters = addOrReplaceFilters([...draft.filters], payload);
+        }),
+
+    [EVENTS_PLANNING.ACTIONS.RECEIVE_EVENTS_PLANNING_FILTERS]: (state, payload) =>
+        produce(state, (draft) => {
+            draft.filters = sortBy([...payload], [(f) => f.name.toLowerCase()]);
+        }),
+
+    [EVENTS_PLANNING.ACTIONS.SELECT_EVENTS_PLANNING_FILTER]: (state, payload: ISearchFilter['_id']) =>
+        produce(state, (draft) => {
+            draft.currentFilter = payload === EVENTS_PLANNING.FILTER.ALL_EVENTS_PLANNING ? null : payload;
+        }),
+
     [MAIN.ACTIONS.CLEAR_SEARCH]: (state, payload: keyof IMainState['search']) => (
         payload !== 'COMBINED' ? state : {
             ...state,
