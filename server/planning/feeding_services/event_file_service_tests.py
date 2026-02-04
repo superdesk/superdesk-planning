bc075a453e72a3776c5e4dcf08f4ed2abb66494e
MarkLark86 <mark.pittaway@gmail.com>
2026-02-05 09:07:28 +1100

    [SDESK-7856] fix: Missing async functionality in feeding services (#2710)

--- server/planning/feeding_services/event_file_service_tests.py
+++ server/planning/feeding_services/event_file_service_tests.py
@@ -8,17 +8,13 @@ get_sorted_files = object()
 
 
 class EventFileFeedingServiceTestCase(TestCase):
-    def setUp(self):
-        super().setUp()
-
     @patch("planning.feeding_services.event_file_service.os")
     @patch("planning.feeding_services.event_file_service.get_sorted_files")
     async def test_update(self, mock_os, mock_get_sorted_files):
-        async with self.app.app_context():
-            service = EventFileFeedingService()
-            provider = {"feed_parser": "ics20", "config": {"path": "/test_file_drop"}}
-            mock_get_sorted_files.return_value = ["file1.txt", "file2.txt", "file3.txt"]
-            mock_os.path.isfile.return_value = True
+        service = EventFileFeedingService()
+        provider = {"feed_parser": "ics20", "config": {"path": "/test_file_drop"}}
+        mock_get_sorted_files.return_value = ["file1.txt", "file2.txt", "file3.txt"]
+        mock_os.path.isfile.return_value = True
 
-            events = list(service._update(provider, None))
-            self.assertEqual(len(events), 0)
+        events = [event async for event in service._update(provider, None)]
+        self.assertEqual(len(events), 0)
