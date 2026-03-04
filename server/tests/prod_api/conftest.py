46ea2cd687c398f951b0fd076d3f2b3d0c7c30af
Mark Pittaway <mark.pittaway@gmail.com>
2026-03-04 14:02:44 +1100

    [SDBELGA-1047] fix: ProdAPI Event pagination fails with planning_source

--- server/tests/prod_api/conftest.py
+++ server/tests/prod_api/conftest.py
@@ -4,28 +4,25 @@ from pathlib import Path
 from flask import json
 from eve.methods.common import parse
 
+from planning.assignments.assignments import assignments_schema
+from planning.prod_api.assignments.resource import AssignmentsResource
+from planning.planning.planning import planning_schema
+from planning.prod_api.planning.resource import PlanningResource
+
 from prod_api.conftest import get_test_prodapi_app, teardown_app
 
 
 @pytest.fixture(scope="function")
-def prodapi_app_with_data(request):
-    """
-    Override the base fixture to use planning fixtures instead of the superdesk-core ones.
-    """
+def prodapi_app(request):
     extra_config = getattr(request, "param", {})
     extra_config["PRODAPI_AUTH_ENABLED"] = False
-    app = get_test_prodapi_app(extra_config)
 
-    # fill with data from our fixtures
-    with app.app_context():
-        p = Path(os.path.join(os.path.dirname(__file__), "tests/fixtures"))
-        for fixture_file in sorted(p.iterdir()):
-            if fixture_file.is_file():
-                with fixture_file.open() as f:
-                    resource = fixture_file.stem
-                    values = json.load(f)
-                    docs = [parse(value, resource) for value in values]
-                    app.data.insert(resource=resource, docs=docs)
+    # patch Assignment, Planning schemas so elasticsearch will get initialised.
+    # this will not happen in a production environment as the index/types should already be created
+    AssignmentsResource.schema = assignments_schema
+    PlanningResource.schema = planning_schema
+
+    app = get_test_prodapi_app(extra_config)
 
     def test_app_teardown():
         teardown_app(app)
@@ -36,10 +33,30 @@ def prodapi_app_with_data(request):
 
 
 @pytest.fixture(scope="function")
-def prodapi_app_with_data_client(prodapi_app_with_data):
+def prodapi_app_with_data(prodapi_app):
+    """
+    Override the base fixture to use planning fixtures instead of the superdesk-core ones.
+    """
+
+    # fill with data from our fixtures
+    with prodapi_app.app_context():
+        p = Path(os.path.join(os.path.dirname(__file__), "tests/fixtures"))
+        for fixture_file in sorted(p.iterdir()):
+            if fixture_file.is_file():
+                with fixture_file.open() as f:
+                    resource = fixture_file.stem
+                    values = json.load(f)
+                    docs = [parse(value, resource) for value in values]
+                    prodapi_app.data.insert(resource=resource, docs=docs)
+
+    return prodapi_app
+
+
+@pytest.fixture(scope="function")
+def prodapi_app_with_data_client(prodapi_app):
     """Test client for prod api with filled data"""
 
-    client = prodapi_app_with_data.test_client()
+    client = prodapi_app.test_client()
 
-    with prodapi_app_with_data.app_context():
+    with prodapi_app.app_context():
         yield client
