09f914777114ef17227c811ab6392de25c992dd9
Mark Pittaway <mark.pittaway@gmail.com>
2026-08-13 14:35:41 +1000

    fix cyclic imports

--- server/planning/__init__.py
+++ server/planning/__init__.py
@@ -7,14 +7,6 @@ from quart_babel import lazy_gettext
 
 from . import settings
 from .agendas import AgendasResource, AgendasService
-from .planning_export_templates import (
-    PlanningExportTemplatesResource,
-    PlanningExportTemplatesService,
-)
-from .planning_article_export import (
-    PlanningArticleExportResource,
-    PlanningArticleExportService,
-)
 from .common import (
     get_max_recurrent_events,
     get_street_map_url,
@@ -69,6 +61,14 @@ import planning.output_formatters  # noqa
 import planning.io  # noqa
 import planning.content_api.output_formatters  # noqa  - Included so ContentAPI formatters are registered
 from planning.search.planning_autocomplete import init_app as init_planning_autocomplete_app
+from .planning_export_templates import (
+    PlanningExportTemplatesResource,
+    PlanningExportTemplatesService,
+)
+from .planning_article_export import (
+    PlanningArticleExportResource,
+    PlanningArticleExportService,
+)
 
 from .module import module  # noqa
 
