ef3acdbc0a9479ce8c39f705727c43b444c3b5ad
Helmy Giacoman <helmygb@gmail.com>
2026-09-14 18:17:21 +0200

    [SDBELGA-1126] Make the remaining unified planning indexes non-unique

--- server/planning/unified/module.py
+++ server/planning/unified/module.py
@@ -36,31 +36,37 @@ unified_planning_resource_config = ResourceConfig(
                 name="recurrence_id_1",
                 keys=[("recurrence_id", 1)],
                 background=True,
+                unique=False,
             ),
             MongoIndexOptions(
                 name="state",
                 keys=[("state", 1)],
                 background=True,
+                unique=False,
             ),
             MongoIndexOptions(
                 name="dates_start_1",
                 keys=[("dates.start", 1)],
                 background=True,
+                unique=False,
             ),
             MongoIndexOptions(
                 name="dates_end_1",
                 keys=[("dates.end", 1)],
                 background=True,
+                unique=False,
             ),
             MongoIndexOptions(
                 name="template",
                 keys=[("template", 1)],
                 background=True,
+                unique=False,
             ),
             MongoIndexOptions(
                 name="planning_recurrence_id",
                 keys=[("planning_recurrence_id", 1)],
                 background=True,
+                unique=False,
             ),
         ]
     ),
