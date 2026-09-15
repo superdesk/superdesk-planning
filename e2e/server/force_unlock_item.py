bfea2f1b41ad05e18272c552a8f898d129989be4
Helmy Giacoman <helmygb@gmail.com>
2026-09-14 22:08:59 -0700

    [SDBELGA-1114] Port the e2e force unlock helper and notify the unlocking session on unlock (#2982)

--- e2e/server/force_unlock_item.py
+++ e2e/server/force_unlock_item.py
@@ -9,27 +9,43 @@
 # at https://www.sourcefabric.org/superdesk/license
 
 from bson import ObjectId
-from superdesk import blueprint, get_resource_service
-from superdesk.flask import Blueprint
-from planning.item_lock import LockService
-from apps.common.components.utils import get_component
+from quart_babel import gettext
+
+from superdesk import blueprint
+from superdesk.errors import SuperdeskApiError
+from superdesk.flask import Blueprint, g
+from superdesk.resource_fields import STATUS, STATUS_OK
+
+from planning.types import AssignmentResourceModel, UnifiedPlanningResource
+from planning.locks.unlock import unlock_item
 
 
-# TODO-ASYNC: migrate to async endpoint
 bp = Blueprint("e2e_force_unlock", __name__)
+
+RESOURCE_MODELS = {
+    "events": UnifiedPlanningResource,
+    "planning": UnifiedPlanningResource,
+    "assignments": AssignmentResourceModel,
+}
 
 
 @bp.route("/e2e/force_unlock/<item_type>/<item_id>", methods=["DELETE"])
-def force_unlock_item(item_type, item_id):
-    original = get_resource_service(item_type).find_one(req=None, _id=item_id)
-    lock_service = get_component(LockService)
+async def force_unlock_item(item_type, item_id):
+    resource_model = RESOURCE_MODELS.get(item_type)
+    if resource_model is None:
+        raise SuperdeskApiError.badRequestError(gettext("Unknown item type"))
 
-    # We can use mocked IDs here, as we aren't actually checking these against real users
-    # merely just sending them througn the websocket notifications
-    user_id = ObjectId()
-    session_id = ObjectId()
+    item = await resource_model.get_service().find_by_id(item_id)
+    if item is None:
+        raise SuperdeskApiError.notFoundError(gettext("Item not found"))
 
-    return lock_service.unlock(original, user_id, session_id, item_type)
+    # Mocked administrator and session: never checked against real users, they only end up in the unlock
+    # notification, which is how the client tells a foreign unlock from its own
+    g.user = {"_id": ObjectId(), "user_type": "administrator"}
+    g.auth = {"_id": ObjectId()}
+    await unlock_item(item)
+
+    return {STATUS: STATUS_OK}
 
 
 def init_app(app):
