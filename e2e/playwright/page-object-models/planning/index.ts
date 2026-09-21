7594044ff07d4534e9d9778e2e54529157a866d9
Helmy Giacoman <helmygb@gmail.com>
2026-09-21 10:09:52 +0200

    [SDESK-8013] Hide "Add to Planning" for articles already linked to a coverage

--- e2e/playwright/page-object-models/planning/index.ts
+++ e2e/playwright/page-object-models/planning/index.ts
@@ -4,6 +4,8 @@ export {ManageContentProfiles} from './manageContentProfiles';
 export {NewLocationPopup} from './newLocationPopup';
 export {AdvancedSearch} from './advancedSearch';
 export {SearchFilters} from './searchFilters';
+export {Monitoring} from './monitoring';
+export {AddToPlanningModal} from './addToPlanningModal';
 
 export * from './assignments';
 export * from './events';
